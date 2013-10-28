009340bc59ec24a4d48accb828f8b2140bf279ba
Frederik Hegger <frederik.hegger@h-brs.de>
2013-10-28 08:41:35 +0100

    change to hydro setup

--- mdr_robots/mdr_bringup/robots/env.sh
+++ mdr_robots/mdr_bringup/robots/env.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-source /opt/ros/fuerte/setup.bash
-export ROS_PACKAGE_PATH=~/fuerte:$ROS_PACKAGE_PATH
+source ~/hydro/devel/setup.bash
+
 export ROSCONSOLE_FORMAT='[${severity}] [${node}]: ${message}'
+
 export ROBOT=cob3-1
-export ROBOT_ENV=eindhoven-2013
-#export ROBOT_ENV=brsu-c069
+
+export ROBOT_ENV=brsu-c069
 #export ROBOT_ENV=ipa-apartment
+
 export ROS_MASTER_URI=http://cob3-1-pc1:11311
 
 exec "$@"
