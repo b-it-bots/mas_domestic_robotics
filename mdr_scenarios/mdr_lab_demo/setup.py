17435b68913852bb30119a260203792e3a84ec8c
Frederik Hegger <frederik.hegger@h-brs.de>
2013-10-30 17:48:01 +0100

    add setup.py

---
+++ mdr_scenarios/mdr_lab_demo/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import distutils.core
+import catkin_pkg.python_setup
+
+d = catkin_pkg.python_setup.generate_distutils_setup(
+   packages=['mdr_lab_demo'],
+   package_dir={'mdr_lab_demo': 'ros/src'}
+)
+
+distutils.core.setup(**d)
