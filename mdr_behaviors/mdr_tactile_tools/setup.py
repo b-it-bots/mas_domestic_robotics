8d5d1224b61c584ba820af34cc676e500802fc02
jsanch2s <jose.sanchez.loza@gmail.com>
2014-03-03 04:55:26 +0100

    add package to evaluate if an object is being grasped

---
+++ mdr_behaviors/mdr_tactile_tools/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+d = generate_distutils_setup(
+    packages=['mdr_tactile_tools', 'mdr_tactile_tools_common'],
+    package_dir={'mdr_tactile_tools': 'ros/src',
+                 'mdr_tactile_tools_common': 'common/src'}
+)
+
+setup(**d)
