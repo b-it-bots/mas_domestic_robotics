5dc016bbcbaf0b7a26f34ecdca9d0ac14636aafc
Argentina Ortega Sainz <argentina.ortega@h-brs.de>
2017-09-18 13:02:11 +0200

    Add mcr_question_matching from mas_common_robotics

--- mdr_speech/mdr_question_matching/setup.py
+++ mdr_speech/mdr_question_matching/setup.py
@@ -4,8 +4,8 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-   packages=['mcr_question_matching'],
-   package_dir={'mcr_question_matching': 'ros/src/mcr_question_matching'}
+   packages=['mdr_question_matching'],
+   package_dir={'mdr_question_matching': 'ros/src/mdr_question_matching'}
 )
 
 setup(**d)
