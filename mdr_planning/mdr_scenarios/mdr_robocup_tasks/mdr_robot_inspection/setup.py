c72cc3cda89b73eb7dc417996425cab75154b3c0
Alex Mitrevski <aleksandar.mitrevski@h-brs.de>
2017-09-29 18:43:03 +0200

    [mdr_robot_inspection] Fixed the python package setup

--- mdr_planning/mdr_scenarios/mdr_robocup_tasks/mdr_robot_inspection/setup.py
+++ mdr_planning/mdr_scenarios/mdr_robocup_tasks/mdr_robot_inspection/setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
    packages=['mdr_robot_inspection'],
-   package_dir={'mdr_robot_inspection': 'src/mdr_robot_inspection'}
+   package_dir={'mdr_robot_inspection': 'ros/src/mdr_robot_inspection'}
 )
 
 setup(**d)
