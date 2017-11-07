5afc0ce4fb62fdd83a7029f92b21dbc553ce2d44
Gabriela Cortes <gabriela.cortes@smail.inf.h-brs.de>
2017-11-07 13:50:11 +0100

    Detect person action working

--- mdr_planning/mdr_actions/mdr_perception_actions/mdr_detect_person/src/mdr_detect_person_action/action_states.py
+++ mdr_planning/mdr_actions/mdr_perception_actions/mdr_detect_person/src/mdr_detect_person_action/action_states.py
@@ -1,16 +1,18 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 
 import rospy
 import smach
 import smach_ros
 import actionlib
 import cv2
+import numpy as np
 
 from mdr_detect_person.msg import DetectPersonFeedback, DetectPersonGoal, DetectPersonResult
 from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
 
-from inference import detect_faces
-from inference import load_detection_model
+from mdr_detect_person_action.inference import detect_faces
+from mdr_detect_person_action.inference import load_detection_model
 
 
 class SetupDetectPerson(smach.State):
@@ -21,44 +23,63 @@ class SetupDetectPerson(smach.State):
 
     def execute(self, userdata):
         start = userdata.detect_person_goal.start
-
-        feedback = DetectPersonFeedback()
-        feedback.current_state = 'DETECT_PERSON'
-        feedback.text = '[detect_person] detecting faces '
-        userdata.detect_person_feedback = feedback
-
-        return 'succeeded'
+        print (start)
+        if start:
+            feedback = DetectPersonFeedback()
+            feedback.current_state = 'DETECT_PERSON'
+            feedback.text = '[detect_person] detecting faces '
+            userdata.detect_person_feedback = feedback
+            return 'succeeded'
+        else:
+            return 'failed'
+                
 
 class DetectPerson(smach.State):
     def __init__(self):
         smach.State.__init__(self, input_keys=['detect_person_goal'], outcomes=['succeeded', 'failed'])
-        rospy.Subscriber("/cam3d/rgb/image_raw", Image, self.callback)
-        
-    def execute(self, userdata):
-        # model for 
-        detection_model_path = '../trained_models/detection_models/haarcascade_frontalface_default.xml'
+        self.bridge = CvBridge()
+        self.image_subscriber = rospy.Subscriber("/cam3d/rgb/image_raw", Image, self.callback)
+                # model for 
+        detection_model_path = '/home/gabych/face_classification/trained_models/detection_models/haarcascade_frontalface_default.xml'
 
         # loading models
-        face_detection = load_detection_model(detection_model_path)
+        self.face_detection = load_detection_model(detection_model_path)
+        
 
-        for i in xrange(1,1000):
-            bgr_image = self.video_capture.read()[1]
-            gray_image = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2GRAY)
-            rgb_image = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2RGB)
-            faces = detect_faces(face_detection, gray_image)
+    def execute(self, userdata):
 
-        if np.size(faces) != 0:
+        for i in iter(range(1,1000)): #xrange(1,1000):
+            try:
+                bgr_image = self.video_capture#.read()[1]
+                gray_image = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2GRAY)
+                #rgb_image = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2RGB)
+                faces = detect_faces(self.face_detection, gray_image)
+                number_of_faces = np.size(faces,0)
+                print (number_of_faces)
+            except:
+                number_of_faces = 0
+                print ("Ops")
 
+        if number_of_faces != 0:
             feedback =  DetectPersonFeedback()
             feedback.faces = faces
             userdata.detect_person_feedback = feedback
             return 'succeeded'
         else:
-            return 'failure'
+            return 'failed'
 
     def callback(self, data):
-        rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
-        self.video_capture = data.data
+        #rospy.loginfo(rospy.get_caller_id() + "I heard %s")# data)
+        self.video_capture = self.convert_image(data)
+        #print (type(self.video_capture), "videocapture")
+
+    def convert_image(self, ros_image):
+        # Use cv_bridge() to convert the ROS image to OpenCV format
+        #print (type(ros_image))  
+        cv_image = self.bridge.imgmsg_to_cv2(ros_image, "bgr8")     
+
+        return np.array(cv_image, dtype=np.uint8)
+
 
 class SetActionLibResult(smach.State):
     def __init__(self, result):
