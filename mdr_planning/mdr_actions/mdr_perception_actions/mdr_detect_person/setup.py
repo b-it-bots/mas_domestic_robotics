ec0914094775a1dab506a451ad8f6311b675738b
Gabriela Cortes <gabriela.cortes@smail.inf.h-brs.de>
2017-11-07 15:35:07 +0100

    Code changed to pass roslint test

--- mdr_planning/mdr_actions/mdr_perception_actions/mdr_detect_person/setup.py
+++ mdr_planning/mdr_actions/mdr_perception_actions/mdr_detect_person/setup.py
@@ -4,8 +4,8 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-   packages=['mdr_detect_person_action'],
-   package_dir={'mdr_detect_person_action': 'src/mdr_detect_person_action'}
+   packages=['mdr_detect_person'],
+   package_dir={'mdr_detect_person': 'ros/src/mdr_detect_person'}
 )
 
 setup(**d)
