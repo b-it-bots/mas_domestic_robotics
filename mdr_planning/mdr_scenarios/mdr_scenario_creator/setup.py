3ff05502077f1ab64801213a7a9f84396e2e6909
Alex Mitrevski <aleksandar.mitrevski@h-brs.de>
2018-03-18 11:08:13 +0100

    mv mdr_scenario_loader mdr_scenario_creator

--- mdr_planning/mdr_scenarios/mdr_scenario_creator/setup.py
+++ mdr_planning/mdr_scenarios/mdr_scenario_creator/setup.py
@@ -4,8 +4,8 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-   packages=['mdr_scenario_loader'],
-   package_dir={'mdr_scenario_loader': 'ros/src/mdr_scenario_loader'}
+   packages=['mdr_scenario_creator'],
+   package_dir={'mdr_scenario_creator': 'ros/src/mdr_scenario_creator'}
 )
 
 setup(**d)
