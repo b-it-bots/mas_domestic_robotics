b711bb035b686105bcdb7eeb412ac0b0509571eb
Alex Mitrevski <aleksandar.mitrevski@h-brs.de>
2018-03-19 16:03:17 +0100

    mv mdr_scenario_creator mdr_execution_manager

--- mdr_planning/mdr_scenarios/mdr_execution_manager/setup.py
+++ mdr_planning/mdr_scenarios/mdr_execution_manager/setup.py
@@ -4,8 +4,8 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-   packages=['mdr_scenario_creator'],
-   package_dir={'mdr_scenario_creator': 'ros/src/mdr_scenario_creator'}
+   packages=['mdr_execution_manager'],
+   package_dir={'mdr_execution_manager': 'ros/src/mdr_execution_manager'}
 )
 
 setup(**d)
