de8d91bc6fa8e4b63e3a9f368b12bde18b4fcf15
Alex Mitrevski <aleksandar.mitrevski@h-brs.de>
2018-03-24 20:14:00 +0100

    [mdr_store_groceries] Added a package for the store groceries scenario

---
+++ mdr_planning/mdr_scenarios/mdr_robocup_tasks/mdr_store_groceries/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+d = generate_distutils_setup(
+   packages=['mdr_store_groceries'],
+   package_dir={'mdr_store_groceries': 'ros/src/mdr_store_groceries'}
+)
+
+setup(**d)
