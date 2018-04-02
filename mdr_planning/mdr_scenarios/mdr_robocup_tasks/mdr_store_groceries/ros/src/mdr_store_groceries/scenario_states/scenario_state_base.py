2f989c507b7dd5b78d414e8a136df2a471655bdd
Alex Mitrevski <aleksandar.mitrevski@h-brs.de>
2018-04-02 16:27:14 +0200

    [mdr_store_groceries] Implemented state saving in the scenario states

--- mdr_planning/mdr_scenarios/mdr_robocup_tasks/mdr_store_groceries/ros/src/mdr_store_groceries/scenario_states/scenario_state_base.py
+++ mdr_planning/mdr_scenarios/mdr_robocup_tasks/mdr_store_groceries/ros/src/mdr_store_groceries/scenario_states/scenario_state_base.py
@@ -5,12 +5,19 @@ import rosplan_dispatch_msgs.msg as plan_dispatch_msgs
 import rosplan_knowledge_msgs.srv as rosplan_srvs
 from mongodb_store.message_store import MessageStoreProxy
 
+from mdr_monitoring_msgs.msg import ExecutionState
+
 class ScenarioStateBase(smach.State):
-    def __init__(self, action_name, outcomes, input_keys=list(), output_keys=list()):
+    def __init__(self, action_name, outcomes,
+                 input_keys=list(), output_keys=list(),
+                 save_sm_state=False):
         smach.State.__init__(self, outcomes=outcomes,
                              input_keys=input_keys,
                              output_keys=output_keys)
+        self.sm_id = ''
+        self.state_name = ''
         self.action_name = action_name
+        self.save_sm_state = save_sm_state
         self.retry_count = 0
         self.executing = False
         self.succeeded = False
@@ -41,6 +48,16 @@ class ScenarioStateBase(smach.State):
 
     def execute(self, userdata):
         pass
+
+    def save_current_state(self):
+        execution_state_msg = ExecutionState()
+        execution_state_msg.stamp = rospy.Time.now()
+        execution_state_msg.state_machine = self.sm_id
+        execution_state_msg.state = self.state_name
+        try:
+            self.msg_store_client.insert_named('current_state', execution_state_msg)
+        except:
+            rospy.logerr('Error while saving current state')
 
     def get_dispatch_msg(self):
         pass
