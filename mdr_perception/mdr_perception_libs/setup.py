659a02f496796831d5448a0afa464675a968d182
Minh Nguyen <minhnh.91@gmail.com>
2018-04-05 11:05:18 +0200

    add mdr_perception_libs python wrapper for cpp library

--- mdr_perception/mdr_perception_libs/setup.py
+++ mdr_perception/mdr_perception_libs/setup.py
@@ -3,7 +3,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
     packages=['mdr_perception_libs'],
-    package_dir={'mdr_perception_libs': 'ros/src/mdr_perception_libs'}
+    package_dir={'': 'ros/src'}
 )
 
 setup(**d)
