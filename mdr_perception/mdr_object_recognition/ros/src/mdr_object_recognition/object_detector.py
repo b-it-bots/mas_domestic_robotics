d098b65512540e3c6bc6b41ea2afe29f33c9d478
Minh Nguyen <minhnh.91@gmail.com>
2018-04-06 16:48:38 +0200

    update ObjectDetector and DetectionServiceProxy
    - use PlaneList message for detection result
    - remove topic subscription and publication from ObjectDetector

--- mdr_perception/mdr_object_recognition/ros/src/mdr_object_recognition/object_detector.py
+++ mdr_perception/mdr_object_recognition/ros/src/mdr_object_recognition/object_detector.py
@@ -1,7 +1,6 @@
 import rospy
-from sensor_msgs.msg import Image
-import std_msgs
 from .detection_service_proxy import DetectionServiceProxy
+from mcr_perception_msgs.msg import PlaneList
 
 
 class ObjectDetector(object):
@@ -10,17 +9,11 @@ class ObjectDetector(object):
             raise ValueError('argument 1 is not a DetectionServiceProxy instance')
 
         self._detection_service_proxy = detection_service_proxy
-        self._event_in_sub = rospy.Subscriber('~event_in', std_msgs.msg.String, self._event_in_cb)
-        self._img_pub = rospy.Publisher('~first_object', Image, queue_size=1)
         pass
 
-
-    def _event_in_cb(self, event):
-        object_list, plane_list = self._detection_service_proxy.get_objects_and_planes()
-        if len(object_list.objects) == 0:
-            return
-
-        self._img_pub.publish(object_list.objects[0].rgb_image)
-        pass
-
+    def detect_objects(self):
+        plane_list = self._detection_service_proxy.get_objects_and_planes()
+        if not isinstance(plane_list, PlaneList):
+            raise ValueError('get_objects_and_planes() did not return a PlaneList instance')
+        return plane_list
 
