f0313cf62856e59cc9ba5646b2efffe19f072c40
Minh Nguyen <minhnh.91@gmail.com>
2018-04-15 09:52:26 +0200

    pull changes from old branch to fix rebase issue
    - add dynamic loading of ServiceProxy class in action
    - add test server, service proxy and launch file for sanity check
    - update README
    - apply various code cleanups

--- mdr_perception/mdr_object_recognition/ros/src/mdr_object_recognition/__init__.py
+++ mdr_perception/mdr_object_recognition/ros/src/mdr_object_recognition/__init__.py
@@ -1,2 +1,2 @@
-from mdr_object_recognition.object_detector import ObjectDetector
-from mdr_object_recognition.detection_service_proxy import DetectionServiceProxy
+from object_detector import ObjectDetector
+from detection_service_proxy import DetectionServiceProxy, DetectionServiceProxyTest, EMPTY_DETECTION_SERVICE
