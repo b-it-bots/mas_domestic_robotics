ec825c7461e1804a90d598183d35a2e259f3c33b
Alex Mitrevski <aleksandar.mitrevski@h-brs.de>
2018-04-21 22:59:12 +0200

    [mdr_robot_inspection] Added a generic robot inspection package with some missing states (that should be implemented for each robot)

---
+++ mdr_planning/mdr_scenarios/mdr_robocup_tasks/mdr_robot_inspection/ros/src/mdr_robot_inspection/scenario_states/move_base.py
@@ -0,0 +1,46 @@
+import rospy
+import actionlib
+
+from mdr_move_base_action.msg import MoveBaseAction, MoveBaseGoal
+
+from mdr_robot_inspection.scenario_states.scenario_state_base import ScenarioStateBase
+
+class MoveBase(ScenarioStateBase):
+    def __init__(self, save_sm_state=False, **kwargs):
+        ScenarioStateBase.__init__(self, 'move_base',
+                                   save_sm_state=save_sm_state,
+                                   outcomes=['succeeded', 'failed', 'failed_after_retrying'])
+        self.sm_id = kwargs.get('sm_id', 'mdr_robot_inspection')
+        self.state_name = kwargs.get('state_name', 'move_base')
+        self.action_server = kwargs.get('action_server', 'move_base_server')
+        self.destination_locations = list(kwargs.get('destination_locations', list()))
+        self.timeout = kwargs.get('timeout', 120.)
+        self.number_of_retries = kwargs.get('number_of_retries', 0)
+        self.retry_count = 0
+        self.client = actionlib.SimpleActionClient(self.action_server, MoveBaseAction)
+        self.client.wait_for_server()
+
+    def execute(self, userdata):
+        if self.save_sm_state:
+            self.save_current_state()
+
+        goal = MoveBaseGoal()
+        for destination_location in self.destination_locations:
+            goal.destination_location = destination_location
+
+            # calling the actionlib server and waiting for the execution to end
+            rospy.loginfo('[move_base] Sending action lib goal to move_base_server,' +
+                          ' destination: ' + goal.destination_location)
+            self.client.send_goal(goal)
+            self.client.wait_for_result(rospy.Duration.from_sec(int(self.timeout)))
+            success = self.client.get_result()
+
+            if success:
+                rospy.loginfo('Successfully reached %s' % destination_location)
+            else:
+                rospy.logerr('Could not reach %s' % destination_location)
+                if self.retry_count == self.number_of_retries:
+                    return 'failed_after_retrying'
+                self.retry_count += 1
+                return 'failed'
+        return 'succeeded'
