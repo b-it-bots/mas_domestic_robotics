00bdfe7a3812b068b8abecb6a32dfa599d025389
Minh Nguyen <minhnh.91@gmail.com>
2018-05-11 10:58:19 +0200

    separate perception_libs from mcr_scene_segmentation

---
+++ mdr_perception/mdr_perception_libs/common/include/mdr_perception_libs/aliases.h
@@ -0,0 +1,17 @@
+//
+// Created by Minh Nguyen on 05.05.18.
+//
+
+#ifndef MDR_PERCEPTION_LIBS_ALIASES_H
+#define MDR_PERCEPTION_LIBS_ALIASES_H
+
+#include <vector>
+#include <pcl/point_types.h>
+#include <pcl/point_cloud.h>
+//#include <pcl/geometry/planar_polygon.h>
+//#include <pcl/segmentation/planar_region.h>
+
+typedef pcl::PointXYZRGB PointT;
+typedef pcl::PointCloud<PointT> PointCloud;
+
+#endif //MDR_PERCEPTION_LIBS_ALIASES_H
