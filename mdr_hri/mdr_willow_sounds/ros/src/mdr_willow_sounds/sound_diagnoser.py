e147a1f7a6666f381ffecac0fb4e26829479f822
jcmayoral <jocamaba1989@gmail.com>
2018-05-17 16:03:34 +0200

    sound_monitor topic message requirement integrated, catch exception for not found requests

--- mdr_hri/mdr_willow_sounds/ros/src/mdr_willow_sounds/sound_diagnoser.py
+++ mdr_hri/mdr_willow_sounds/ros/src/mdr_willow_sounds/sound_diagnoser.py
@@ -8,7 +8,7 @@ import rospy
 import rospkg
 from dynamic_reconfigure.server import Server
 from mdr_willow_sounds.cfg import soundDiagnoserConfig
-
+from std_msgs.msg import String
 
 class SoundDiagnoser:
     def __init__(self, config_file):
@@ -16,16 +16,17 @@ class SoundDiagnoser:
         rospy.init_node(package_name + '_node', anonymous = False)
 
         self.package_path = rospkg.RosPack().get_path(package_name) + '/'
-        sound_dictionary = yaml.load(open(self.package_path + "config/" + config_file+".yaml"))
+        self.sound_dictionary = yaml.load(open(self.package_path + "config/" + config_file+".yaml"))
         self.CHUNK = 1024 #TODO
 
         self.is_enable = True
         self.audio_manager = pyaudio.PyAudio()
 
-        for sound in sound_dictionary:
-            sound_file = sound_dictionary[sound]['folder']+'/'+ sound_dictionary[sound]['file_name']
-            rospy.Subscriber(sound_dictionary[sound]['topic'], rospy.AnyMsg, self.mainCB, sound_file)
+        for sound in self.sound_dictionary:
+            sound_file = self.sound_dictionary[sound]['folder']+'/'+ self.sound_dictionary[sound]['file_name']
+            rospy.Subscriber(self.sound_dictionary[sound]['topic'], rospy.AnyMsg, self.mainCB, sound_file)
 
+        rospy.Subscriber('sound_monitor', String, self.soundCB)
         self.dyn_reconfigure_srv = Server(soundDiagnoserConfig, self.dynamic_reconfigureCB)
 
         rospy.spin()
@@ -33,6 +34,17 @@ class SoundDiagnoser:
     def dynamic_reconfigureCB(self,config, level):
         self.is_enable = config["allow_sound"]
         return config
+
+    def soundCB(self,msg):
+        sound = msg.data
+
+        try:
+            sound_file = self.sound_dictionary[sound]['folder']+'/'+ self.sound_dictionary[sound]['file_name']
+            sound_file_path = self.package_path + 'willow-sound/'+ sound_file
+            if self.is_enable:
+                self.playSound(sound_file_path)
+        except Exception:
+            rospy.logerr('Required Sound does not exists')
 
     def mainCB(self, msg, sound_file):
         sound_file_path = self.package_path + 'willow-sound/'+ sound_file
