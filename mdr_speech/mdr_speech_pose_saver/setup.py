d4648cad7eb895e25e241bc6a9c385ecc6a65d4f
jcmayoral <jocamaba1989@gmail.com>
2018-06-14 11:42:52 +0200

    basic functionality tested, string must be filtered before saving pose

--- mdr_speech/mdr_speech_pose_saver/setup.py
+++ mdr_speech/mdr_speech_pose_saver/setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
    packages=['mdr_speech_pose_saver'],
-   package_dir={'mdr_speech_pose_saver': 'ros/src/speech_pose_saver'}
+   package_dir={'mdr_speech_pose_saver': 'ros/src/mdr_speech_pose_saver'}
 )
 
 setup(**d)
