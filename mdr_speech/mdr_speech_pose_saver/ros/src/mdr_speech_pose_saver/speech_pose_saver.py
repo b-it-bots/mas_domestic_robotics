b666b368e7a8577904b20da33b84a3d3f610e66e
jcmayoral <jocamaba1989@gmail.com>
2018-06-17 10:52:13 +0200

    snake_case functions renamed, parametrize of map_frame, robot_frame and speech_input_topic

--- mdr_speech/mdr_speech_pose_saver/ros/src/mdr_speech_pose_saver/speech_pose_saver.py
+++ mdr_speech/mdr_speech_pose_saver/ros/src/mdr_speech_pose_saver/speech_pose_saver.py
@@ -5,17 +5,20 @@ import rospy
 class SpeechPoseSaver(PoseSaver):
     def __init__(self, file_name):
         rospy.init_node("speech_pose_saver")
-        PoseSaver.__init__(self, file_name)
-        rospy.Subscriber("speech_recognizer", String, self.recognizerCB)
+        map_frame = rospy.get_param('~map_frame', 'map')
+        robot_frame = rospy.get_param('~robot_frame', 'base_link')
+        speech_input_topic = rospy.get_param('~speech_input_topic', 'speech_recognizing')
+        PoseSaver.__init__(self, file_name, map_frame, robot_frame)
+        rospy.Subscriber(speech_input_topic, String, self.recognizerCB)
 
     def recognizerCB (self, msg):
         #String must be filter Here
-        self.setPoseName(msg.data)
+        self.set_pose_name(msg.data)
 
-        if not self.isExitRequested():
-            self.savePose()
+        if not self.is_exit_requested():
+            self.save_pose()
 
-    def isExitRequested(self):
+    def is_exit_requested(self):
         if self.pose_name == "exit":
             return True
 
