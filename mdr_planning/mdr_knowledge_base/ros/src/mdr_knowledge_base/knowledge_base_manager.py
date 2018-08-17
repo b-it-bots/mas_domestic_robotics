451d47bd8af46fe510bd86e852c44251096293a9
Alex Mitrevski <aleksandar.mitrevski@h-brs.de>
2018-08-17 18:40:30 +0200

    [knowledge_base_manager.py] Added methods for getting the parent and subclasses of a class

--- mdr_planning/mdr_knowledge_base/ros/src/mdr_knowledge_base/knowledge_base_manager.py
+++ mdr_planning/mdr_knowledge_base/ros/src/mdr_knowledge_base/knowledge_base_manager.py
@@ -34,3 +34,39 @@ class KnowledgeBaseManager(object):
             instance_name = instance[instance.rfind('/')+1:]
             instances.append(instance_name)
         return instances
+
+    def get_subclasses_of(self, class_name):
+        '''Returns a list of all subclasses of 'class_name'
+
+        Keyword arguments:
+        class_name -- string representing the name of a class
+
+        '''
+        rdf_class = self.class_prefix + ':' + class_name
+        rdf_class_uri = rdflib.URIRef(rdf_class)
+        query_result = self.knowledge_graph.transitive_subjects(rdflib.RDFS.subClassOf,
+                                                                rdf_class_uri)
+        subclasses = list()
+        for subclass_uri in query_result:
+            subclass = str(subclass_uri)
+            subclass = subclass[subclass.find(':')+1:]
+            subclasses.append(subclass)
+        return subclasses
+
+    def get_parent_classes_of(self, class_name):
+        '''Returns a list of all parent classes of 'class_name'
+
+        Keyword arguments:
+        class_name -- string representing the name of a class
+
+        '''
+        rdf_class = self.class_prefix + ':' + class_name
+        rdf_class_uri = rdflib.URIRef(rdf_class)
+        query_result = self.knowledge_graph.transitive_objects(rdf_class_uri,
+                                                               rdflib.RDFS.subClassOf)
+        parent_classes = list()
+        for class_uri in query_result:
+            parent_class = str(class_uri)
+            parent_class = parent_class[parent_class.find(':')+1:]
+            parent_classes.append(parent_class)
+        return parent_classes
