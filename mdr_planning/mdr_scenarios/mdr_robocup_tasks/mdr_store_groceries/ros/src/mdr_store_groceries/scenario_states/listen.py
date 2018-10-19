589d54867f0e1288edd6f05d9eb80d0d614a7fa3
PatrickNa <patrick.nagel@smail.inf.h-brs.de>
2018-10-19 17:24:43 +0200

    Add further details for store grocery state machine.
    This commit adds the states listen and process_command to the state machine Also the package.xml and CMakeList have been adjust accordingly. What needs to be done in an upcoming commit is: Create launch files for the actions listen and execute_command and maintain the launch file of the store_groceries launch file.

---
+++ mdr_planning/mdr_scenarios/mdr_robocup_tasks/mdr_store_groceries/ros/src/mdr_store_groceries/scenario_states/listen.py
@@ -0,0 +1,30 @@
+import rospy
+
+from std_msgs.msg import String
+
+from mdr_move_base_action.msg import ListenAction, ListenGoal
+from mas_execution_manager.scenario_state_base import ScenarioStateBase
+
+class Listen(ScenarioStateBase):
+    def __init__(self, save_sm_state=False, **kwargs):
+        ScenarioStateBase.__init__(self, 'listen',
+                                   save_sm_state=save_sm_state,
+                                   outcomes=['received_command', 'failed'])
+
+
+
+        self.listen_client = actionlib.SimpleActionClient('listen_server', ListenAction)
+        self.listen_client.wait_for_server()
+
+
+    def execute(self, userdata):
+        listen_goal = ListenGoal()
+        self.listen_client.send_goal(listen_goal)
+
+        timeout = 15.0
+        self.listen_client.wait_for_result(rospy.Duration.from_sec(int(timeout)))
+        result = self.listen_client.get_result()
+        if result.success:
+            return 'received_command'
+        else:
+            return 'failed'
