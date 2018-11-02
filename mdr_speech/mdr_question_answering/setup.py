7b7fa685edca0b9d31b6504daac65abfc5851d90
Henrik Schnor <hschnor@mailbox.org>
2018-11-02 15:07:00 +0100

    Answer the weather and location question with data from public APIs

--- mdr_speech/mdr_question_answering/setup.py
+++ mdr_speech/mdr_question_answering/setup.py
@@ -4,8 +4,10 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-   packages=['mdr_question_answering'],
-   package_dir={'mdr_question_answering': 'ros/src/mdr_question_answering'}
+   packages=['mdr_question_answering', 'ip_info', 'weather_api'],
+   package_dir={'mdr_question_answering': 'ros/src/mdr_question_answering',
+                'ip_info': 'common/src/ip_info',
+                'weather_api': 'common/src/weather_api'}
 )
 
 setup(**d)
