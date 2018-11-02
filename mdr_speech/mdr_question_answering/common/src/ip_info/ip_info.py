1282f2426f10972066e9de213970681d9322fa05
Henrik Schnor <hschnor@mailbox.org>
2018-11-02 15:03:57 +0100

    Use a public API for IP info retrieval

---
+++ mdr_speech/mdr_question_answering/common/src/ip_info/ip_info.py
@@ -0,0 +1,30 @@
+import requests
+
+
+class IPInfo:
+    """
+    Simple class for retrieving basic information about the currently
+    used IP address (location, provider, timezone)
+    """
+
+    @staticmethod
+    def get():
+        try:
+            return requests.get("http://ip-api.com/json").json()
+        except Exception as e:
+            # Something went wrong
+            return None
+
+    @staticmethod
+    def get_coordinates():
+        ip_info = IPInfo.get()
+        if ip_info is not None and 'lat' in ip_info and 'lon' in ip_info:
+            return '{},{}'.format(ip_info['lat'], ip_info['lon'])
+        return None
+
+    @staticmethod
+    def get_location():
+        ip_info = IPInfo.get()
+        if ip_info is not None and 'city' in ip_info and 'country' in ip_info:
+            return (ip_info['city'], ip_info['country'])
+        return None
