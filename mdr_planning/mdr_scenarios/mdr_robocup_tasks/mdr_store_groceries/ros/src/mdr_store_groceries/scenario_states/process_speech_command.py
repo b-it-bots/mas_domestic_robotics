23d126cd472906e2b9e68561472b0d30535f86af
Alex Mitrevski <aleksandar.mitrevski@h-brs.de>
2018-11-19 08:38:19 +0100

    [store_groceries/process_speech_command] Various error fixes

--- mdr_planning/mdr_scenarios/mdr_robocup_tasks/mdr_store_groceries/ros/src/mdr_store_groceries/scenario_states/process_speech_command.py
+++ mdr_planning/mdr_scenarios/mdr_robocup_tasks/mdr_store_groceries/ros/src/mdr_store_groceries/scenario_states/process_speech_command.py
@@ -8,23 +8,26 @@ class ProcessCommand(ScenarioStateBase):
     def __init__(self, save_sm_state=False, **kwargs):
         ScenarioStateBase.__init__(self, 'listen',
                                    save_sm_state=save_sm_state,
-                                   outcomes=['succeeded', 'failed', 'failed_after_retrying'])
-
+                                   input_keys=['listen_result'],
+                                   outcomes=['start_store_groceries', 'continue_waiting'])
 
         self.start_command = kwargs.get('start_command', 'store groceries')
-        self.process_speech_command_client = actionlib.SimpleActionClient('process_speech_command_server', ProcessSpeechAction)
-        self.process_speech_command_client.wait_for_server()
+        self.process_command_server = kwargs.get('process_command_server',
+                                                 '/mdr_actions/process_speech_command_server')
+        self.process_command_client = actionlib.SimpleActionClient(self.process_command_server,
+                                                                   ProcessSpeechAction)
+        self.process_command_client.wait_for_server()
 
     def execute(self, userdata):
-        process_speech_command_goal = ProcessSpeechGoal()
-        process_speech_command_goal.command = userdata.listen_result.message
-        process_speech_command_goal.start_command = self.start_command
-        self.process_speech_command_client.send_goal(process_speech_command_goal)
+        process_command_goal = ProcessSpeechGoal()
+        process_command_goal.command = userdata.listen_result.message
+        process_command_goal.start_command = self.start_command
+        self.process_command_client.send_goal(process_command_goal)
 
         timeout = 15.0
-        self.process_speech_command_client.wait_for_result(rospy.Duration.from_sec(int(timeout)))
-        result = self.process_speech_command_client.get_result()
-        if result.success:
+        self.process_command_client.wait_for_result(rospy.Duration.from_sec(int(timeout)))
+        result = self.process_command_client.get_result()
+        if result and result.success:
             return 'start_store_groceries'
         else:
             return 'continue_waiting'
