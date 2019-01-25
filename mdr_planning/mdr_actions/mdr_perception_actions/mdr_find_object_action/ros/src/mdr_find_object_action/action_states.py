3f3412ba74ae49535c9c2aaf02dcb3851b6633ee
Alex Mitrevski <aleksandar.mitrevski@h-brs.de>
2019-01-25 16:16:56 +0100

    The find object action is now also implemented using a fault-tolerant state machine

--- mdr_planning/mdr_actions/mdr_perception_actions/mdr_find_object_action/ros/src/mdr_find_object_action/action_states.py
+++ mdr_planning/mdr_actions/mdr_perception_actions/mdr_find_object_action/ros/src/mdr_find_object_action/action_states.py
@@ -1,93 +1,106 @@
 #!/usr/bin/env python
 import rospy
-import smach
+from pyftsm.ftsm import FTSMTransitions
+from mas_execution.action_sm_base import ActionSMBase
 from mas_knowledge_utils.domestic_ontology_interface import DomesticOntologyInterface
 from mas_knowledge_base.domestic_kb_interface import DomesticKBInterface
-from mdr_find_object_action.msg import FindObjectGoal, FindObjectResult, FindObjectFeedback
+from mdr_find_object_action.msg import FindObjectGoal, FindObjectResult
 
-class FindObject(smach.State):
-    def __init__(self, ontology_url, ontology_class_prefix,
-                 number_of_retries=0, timeout=120.):
-        smach.State.__init__(self, outcomes=['succeeded', 'failed', 'timeout'],
-                             input_keys=['find_object_goal'],
-                             output_keys=['find_object_feedback',
-                                          'obj_location', 'relation'])
-        self.ontology_interface = DomesticOntologyInterface(ontology_url,
-                                                            ontology_class_prefix)
-        self.kb_interface = DomesticKBInterface()
-
+class FindObjectSM(ActionSMBase):
+    def __init__(self, ontology_url,
+                 ontology_class_prefix,
+                 number_of_retries=0,
+                 timeout=120.,
+                 max_recovery_attempts=1):
+        super(FindObjectSM, self).__init__('FindObject', [], max_recovery_attempts)
+        self.ontology_url = ontology_url
+        self.ontology_class_prefix = ontology_class_prefix
         self.number_of_retries = number_of_retries
         self.timeout = timeout
+        self.ontology_interface = None
+        self.kb_interface = None
 
-    def execute(self, userdata):
-        if userdata.find_object_goal.goal_type == FindObjectGoal.NAMED_OBJECT:
-            obj_name = userdata.find_object_goal.object_name
+    def init(self):
+        try:
+            rospy.loginfo('[find_object] Creating an interface client for ontology %s', self.ontology_url)
+            self.ontology_interface = DomesticOntologyInterface(self.ontology_url,
+                                                                self.ontology_class_prefix)
+        except Exception as exc:
+            rospy.logerr('[find_object] Could not create an ontology interface client: %s', exc)
+
+        try:
+            rospy.loginfo('[find_object] Creating a knowledge base interface client')
+            self.kb_interface = DomesticKBInterface()
+        except Exception as exc:
+            rospy.logerr('[find_object] Could not create a knowledge base interface client: %s', exc)
+        return FTSMTransitions.INITIALISED
+
+    def running(self):
+        obj_name = None
+        obj_location = None
+        relation = None
+        if self.goal.goal_type == FindObjectGoal.NAMED_OBJECT:
+            obj_name = self.goal.object_name
             location, predicate = self.kb_interface.get_object_location(obj_name)
             if location:
-                rospy.loginfo('[FIND_OBJECT] Found %s %s %s', obj_name, predicate, location)
+                rospy.loginfo('[find_object] Found %s %s %s', obj_name, predicate, location)
                 # TODO: verify that the object is still at that location
-                userdata.obj_location = location
-                userdata.relation = predicate
-                return 'succeeded'
+                obj_location = location
+                relation = predicate
+                self.result = self.set_result(True, obj_location, relation)
+                return FTSMTransitions.DONE
 
-            rospy.loginfo('[FIND_OBJECT] %s not found in the knowledge base; querying the ontology', obj_name)
+            rospy.loginfo('[find_object] %s not found in the knowledge base; querying the ontology', obj_name)
             location = self.ontology_interface.get_default_storing_location(obj_name=obj_name)
             if location:
                 predicate = 'in'
-                rospy.loginfo('[FIND_OBJECT] %s is usually %s %s', obj_name, predicate, location)
+                rospy.loginfo('[find_object] %s is usually %s %s', obj_name, predicate, location)
                 # TODO: check if the object is at the default location
-                userdata.obj_location = location
-                userdata.relation = predicate
-                return 'succeeded'
+                obj_location = location
+                relation = predicate
+                self.result = self.set_result(True, obj_location, relation)
+                return FTSMTransitions.DONE
 
-            rospy.loginfo('[FIND_OBJECT] %s not found', obj_name)
-            userdata.obj_location = None
-            userdata.relation = None
-            return 'failed'
-        elif userdata.find_object_goal.goal_type == FindObjectGoal.OBJECT_CATEGORY:
-            obj_category = userdata.find_object_goal.object_name
+            rospy.loginfo('[find_object] %s not found', obj_name)
+            obj_location = None
+            relation = None
+            self.result = self.set_result(True, obj_location, relation)
+            return FTSMTransitions.DONE
+        elif self.goal.goal_type == FindObjectGoal.OBJECT_CATEGORY:
+            obj_category = self.goal.object_name
             category_objects = self.kb_interface.get_category_objects(obj_category)
             if category_objects:
                 for obj_name in category_objects:
                     location, predicate = self.kb_interface.get_object_location(obj_name)
                     if location:
-                        rospy.loginfo('[FIND_OBJECT] Found %s %s %s', obj_name, predicate, location)
+                        rospy.loginfo('[find_object] Found %s %s %s', obj_name, predicate, location)
                         # TODO: verify that the object is still at that location
-                        userdata.obj_location = location
-                        userdata.relation = predicate
-                        return 'succeeded'
+                        obj_location = location
+                        relation = predicate
+                        self.result = self.set_result(True, obj_location, relation)
+                        return FTSMTransitions.DONE
                     else:
-                        rospy.loginfo('[FIND_OBJECT] The location of %s is unknown', obj_name)
+                        rospy.loginfo('[find_object] The location of %s is unknown', obj_name)
             else:
-                rospy.loginfo('[FIND_OBJECT] No object of category %s was found in the knowledge base; querying the ontology', obj_category)
+                rospy.loginfo('[find_object] No object of category %s was found in the knowledge base; querying the ontology', obj_category)
 
             location = self.ontology_interface.get_default_storing_location(obj_category=obj_category)
             if location:
                 predicate = 'in'
-                rospy.loginfo('[FIND_OBJECT] Objects of %s are usually %s %s', obj_category, predicate, location)
+                rospy.loginfo('[find_object] Objects of %s are usually %s %s', obj_category, predicate, location)
                 # TODO: check if an object of the desired category is at the default location
-                userdata.obj_location = location
-                userdata.relation = predicate
-                return 'succeeded'
+                obj_location = location
+                relation = predicate
+                self.result = self.set_result(True, obj_location, relation)
+                return FTSMTransitions.DONE
 
-            return 'failed'
+            rospy.logerr('[find_object] Object of category %s could not be found', obj_category)
+            self.result = self.set_result(False, obj_location, relation)
+            return FTSMTransitions.DONE
 
-class SetActionLibResult(smach.State):
-    def __init__(self, result):
-        smach.State.__init__(self, outcomes=['succeeded'],
-                             input_keys=['find_object_goal', 'find_object_feedback',
-                                         'obj_location', 'relation'],
-                             output_keys=['find_object_feedback', 'find_object_result'])
-        self.result = result
-
-    def execute(self, userdata):
-        if 'find_object_feedback' not in userdata:
-            userdata.find_object_feedback = FindObjectFeedback()
-        userdata.find_object_feedback.current_state = 'set_action_lib_result'
-
+    def set_result(self, success, obj_location, relation):
         result = FindObjectResult()
-        result.success = self.result
-        result.object_location = userdata.obj_location
-        result.relation = userdata.relation
-        userdata.find_object_result = result
-        return 'succeeded'
+        result.success = success
+        result.object_location = obj_location
+        result.relation = relation
+        return result
