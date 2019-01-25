7b9ba69ffa96bfb893e493974b7b9eb1ebcb382e
Alex Mitrevski <aleksandar.mitrevski@h-brs.de>
2019-01-25 15:37:21 +0100

    The perception actions are now implemented using a fault-tolerant state machine

--- mdr_planning/mdr_actions/mdr_perception_actions/mdr_perceive_plane_action/ros/src/mdr_perceive_plane_action/action_states.py
+++ mdr_planning/mdr_actions/mdr_perception_actions/mdr_perceive_plane_action/ros/src/mdr_perceive_plane_action/action_states.py
@@ -1,38 +1,37 @@
 #!/usr/bin/python
 import rospy
-import smach
-import std_msgs.msg
 import sensor_msgs.msg
-from mdr_perceive_plane_action.msg import PerceivePlaneResult, PerceivePlaneFeedback
+from pyftsm.ftsm import FTSMTransitions
+from mas_execution.action_sm_base import ActionSMBase
 from mcr_perception_msgs.msg import PlaneList
-from mas_perception_libs import ObjectDetector, Constant, ImageRecognitionServiceProxy
+from mas_perception_libs import ObjectDetector, ImageRecognitionServiceProxy
+from mdr_perceive_plane_action.msg import PerceivePlaneResult, PerceivePlaneFeedback
 
-
-class DetectObjects(smach.State):
-    def __init__(self, detection_service_proxy, target_frame=None, timeout_duration=1):
-        smach.State.__init__(self, outcomes=[Constant.SUCCESS, Constant.FAILURE, Constant.TIMEOUT],
-                             input_keys=['perceive_plane_goal', 'perceive_plane_feedback'],
-                             output_keys=['detected_planes', 'perceive_plane_feedback'])
+class PerceivePlaneSM(ActionSMBase):
+    def __init__(self, detection_service_proxy,
+                 recog_service_name,
+                 recog_model_name,
+                 preprocess_input_module,
+                 target_frame=None,
+                 timeout_duration=1,
+                 max_recovery_attempts=1):
+        super(PerceivePlaneSM, self).__init__('PerceivePlane', [], max_recovery_attempts)
         self._detector = ObjectDetector(detection_service_proxy)
+        self._recog_service_proxy = ImageRecognitionServiceProxy(recog_service_name,
+                                                                 recog_model_name,
+                                                                 preprocess_input_module)
+        self._image_pub = rospy.Publisher('/first_recognized_image',
+                                          sensor_msgs.msg.Image,
+                                          queue_size=1)
         self._timeout_duration = timeout_duration
-        self._detecting_done = False
         self._target_frame = target_frame
-
-    def _detection_cb(self):
-        self._detecting_done = True
-
-    def execute(self, ud):
-        if 'perceive_plane_feedback' not in ud:
-            ud.perceive_plane_feedback = PerceivePlaneFeedback()
-        ud.perceive_plane_feedback.current_state = 'detect_objects'
-
         self._detecting_done = False
-        ud.detected_planes = None
-        if 'perceive_plane_goal' not in ud:
-            rospy.logerr('no goal passed into DetectObjects state')
-            return Constant.FAILURE
 
-        self._detector.start_detect_objects(ud.perceive_plane_goal.plane_frame_prefix, self._detection_cb,
+    def running(self):
+        detected_planes = None
+        self._detecting_done = False
+        self._detector.start_detect_objects(self.goal.plane_frame_prefix,
+                                            self._detection_cb,
                                             self._target_frame)
 
         timeout = rospy.Duration.from_sec(self._timeout_duration)
@@ -41,35 +40,19 @@ class DetectObjects(smach.State):
         while (rospy.Time.now() - start_time) < timeout:
             if self._detecting_done:
                 if self._detector.plane_list is None:
-                    return Constant.FAILURE
-
-                ud.detected_planes = self._detector.plane_list
-                return Constant.SUCCESS
+                    rospy.logerr('[perceive_plane] No planes found')
+                    self.result = self.set_result(False, detected_planes)
+                    return FTSMTransitions.DONE
+                detected_planes = self._detector.plane_list
             rate.sleep()
 
-        return Constant.TIMEOUT
+        if not self._detecting_done:
+            rospy.logerr('[perceive_plane] A plane could not be found within the alloted time')
+            self.result = self.set_result(False, detected_planes)
+            return FTSMTransitions.DONE
 
-
-class RecognizeObjects(smach.State):
-    def __init__(self, recog_service_name, recog_model_name, preprocess_input_module):
-        smach.State.__init__(self, outcomes=[Constant.SUCCESS, Constant.FAILURE],
-                             input_keys=['detected_planes', 'perceive_plane_feedback'],
-                             output_keys=['recognized_planes', 'perceive_plane_feedback'])
-
-        self._service_proxy = ImageRecognitionServiceProxy(recog_service_name, recog_model_name,
-                                                           preprocess_input_module)
-        self._image_pub = rospy.Publisher('/first_recognized_image', sensor_msgs.msg.Image, queue_size=1)
-
-    def execute(self, ud):
-        if 'perceive_plane_feedback' not in ud:
-            ud.perceive_plane_feedback = PerceivePlaneFeedback()
-        ud.perceive_plane_feedback.current_state = 'recognize_objects'
-
-        if 'detected_planes' not in ud:
-            ud.recognized_planes = None
-            return Constant.FAILURE
-
-        for plane in ud.detected_planes.planes:
+        rospy.loginfo('[perceive_plane] Found %d objects', len(detected_planes.planes))
+        for plane in detected_planes.planes:
             image_messages = []
             for obj in plane.object_list.objects:
                 image_messages.append(obj.rgb_image)
@@ -79,90 +62,28 @@ class RecognizeObjects(smach.State):
             if len(indices) > 0:
                 obj_index = indices[0]
                 self._image_pub.publish(image_messages[obj_index])
-                rospy.loginfo('first object found: {0} (prob: {1})'.format(classes[obj_index], probs[obj_index]))
+                rospy.loginfo('[perceive_plane] first object found: {0} (prob: {1})'.format(classes[obj_index],
+                                                                                            probs[obj_index]))
             else:
-                rospy.logwarn('no object recognized for plane ' + plane.name)
+                rospy.logwarn('[perceive_plane] no objects recognized for plane %s', plane.name)
 
             for i in indices:
                 plane.object_list.objects[i].name = classes[i]
                 # TODO: handle categories
                 plane.object_list.objects[i].category = classes[i]
 
-        ud.recognized_planes = ud.detected_planes
-        return Constant.SUCCESS
+        self.result = self.set_result(True, detected_planes)
+        return FTSMTransitions.DONE
 
+    def _detection_cb(self):
+        self._detecting_done = True
 
-class SetupPlaneConfig(smach.State):
-    def __init__(self, sleep_duration=1):
-        smach.State.__init__(self, outcomes=[Constant.SUCCESS, Constant.WAITING, Constant.FAILURE, Constant.TIMEOUT],
-                             input_keys=['perceive_plane_goal'],
-                             output_keys=['perceive_plane_feedback'])
-        self.sleep_duration = sleep_duration
-
-        self.config_name_pub = rospy.Publisher("/mcr_common/dynamic_reconfigure_client/configuration_name",
-                                               std_msgs.msg.String, queue_size=1)
-        self.event_in_pub = rospy.Publisher("/mcr_common/dynamic_reconfigure_client/event_in",
-                                            std_msgs.msg.String, queue_size=1)
-        self.event_out_sub = rospy.Subscriber("/mcr_common/dynamic_reconfigure_client/event_out",
-                                              std_msgs.msg.String, self.event_cb)
-        self.event = None
-
-    def event_cb(self, msg):
-        self.event = msg.data
-
-    def configure_plane(self, config_name):
-        """
-        :param config_name: configurations name listed in
-                            config/perceive_plane_configurations.yaml
-        """
-        self.event = None
-        self.config_name_pub.publish(config_name)
-        self.event_in_pub.publish("e_start")
-
-        timeout = rospy.Duration.from_sec(1.0)
-        rate = rospy.Rate(10)
-        start_time = rospy.Time.now()
-        while (rospy.Time.now() - start_time) < timeout:
-            if self.event:
-                if self.event == Constant.E_SUCCESS:
-                    return Constant.SUCCESS
-                return Constant.FAILURE
-            rate.sleep()
-        return Constant.TIMEOUT
-
-    def execute(self, ud):
-        feedback = PerceivePlaneFeedback()
-        feedback.current_state = 'setup_plane_config'
-        if 'perceive_plane_goal' in ud:
-            feedback.message = '[perceive_plane] received plane config goal ' +\
-                               ud.perceive_plane_goal.plane_config
-            ud.perceive_plane_feedback = feedback
-            return self.configure_plane(ud.perceive_plane_goal.plane_config)
-
-        feedback.message = '[perceive_plane] waiting for plane config goal'
-        ud.perceive_plane_feedback = feedback
-        rospy.sleep(self.sleep_duration)
-        return Constant.WAITING
-
-
-class SetActionLibResult(smach.State):
-    def __init__(self, result):
-        smach.State.__init__(self, outcomes=[Constant.SUCCESS],
-                             input_keys=['perceive_plane_goal', 'recognized_planes', 'perceive_plane_feedback'],
-                             output_keys=['perceive_plane_feedback', 'perceive_plane_result'])
-        self.result = result
-
-    def execute(self, ud):
-        if 'perceive_plane_feedback' not in ud:
-            ud.perceive_plane_feedback = PerceivePlaneFeedback()
-        ud.perceive_plane_feedback.current_state = 'set_action_lib_result'
-
+    def set_result(self, success, recognized_planes):
         result = PerceivePlaneResult()
-        result.success = self.result
-        if ud.recognized_planes is None:
+        result.success = success
+        if recognized_planes is None:
             result.recognized_planes = PlaneList()
             result.recognized_planes.planes = []
         else:
-            result.recognized_planes = ud.recognized_planes
-        ud.perceive_plane_result = result
-        return Constant.SUCCESS
+            result.recognized_planes = recognized_planes
+        return result
--- mdr_planning/mdr_actions/mdr_perception_actions/mdr_recognize_emotion_action/ros/src/mdr_recognize_emotion_action/action_states.py
+++ mdr_planning/mdr_actions/mdr_perception_actions/mdr_recognize_emotion_action/ros/src/mdr_recognize_emotion_action/action_states.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python
-
 import rospy
-import smach
-import smach_ros
-import actionlib
 from sensor_msgs.msg import Image
 
 import numpy as np
@@ -12,67 +8,57 @@ import tensorflow as tf
 from keras.models import load_model
 from cv_bridge import CvBridge, CvBridgeError
 
-from mdr_recognize_emotion_action.msg import (RecognizeEmotionFeedback,
-                                              RecognizeEmotionResult)
+from pyftsm.ftsm import FTSMTransitions
+from mas_execution.action_sm_base import ActionSMBase
+from mdr_recognize_emotion_action.msg import RecognizeEmotionFeedback, RecognizeEmotionResult
 
-
-class SetupRecognizeEmotion(smach.State):
-    def __init__(self):
-        smach.State.__init__(self, outcomes=['succeeded', 'failed'],
-                             input_keys=['recognize_emotion_goal'],
-                             output_keys=['recognize_emotion_feedback',
-                                          'recognize_emotion_result'])
-
-    def execute(self, userdata):
-        feedback = RecognizeEmotionFeedback()
-        feedback.current_state = 'RECOGNIZE_EMOTION'
-        feedback.message = '[recognize_emotion] Recongnizing emotion'
-        userdata.recognize_emotion_feedback = feedback
-        return 'succeeded'
-
-
-class RecognizeEmotion(smach.State):
-    def __init__(self, timeout=120.0, image_topic='/cam3d/rgb/image_raw',
-                 emotion_model_path=None, labels=dict(), image_size=(0, 0, 0)):
-        smach.State.__init__(self, input_keys=['recognize_emotion_goal',
-                                               'emotions'],
-                             output_keys=['recognize_emotion_feedback',
-                                          'emotions'],
-                             outcomes=['succeeded', 'failed'])
+class RecognizeEmotionSM(ActionSMBase):
+    def __init__(self, timeout=120.0,
+                 image_topic='/cam3d/rgb/image_raw',
+                 emotion_model_path=None,
+                 labels=None,
+                 image_size=(0, 0, 0),
+                 max_recovery_attempts=1):
+        super(RecognizeEmotionSM, self).__init__('RecognizeEmotion', [], max_recovery_attempts)
         self.timeout = timeout
         self.labels = labels
         self.image_size = image_size
         self.image_publisher = rospy.Publisher(image_topic, Image, queue_size=1)
+        self.emotion_model_path = emotion_model_path
         self.bridge = CvBridge()
+        self.emotion_model = None
+        self.computation_graph = None
 
+    def init(self):
         try:
+            rospy.loginfo('[recognize_emotion] Loading model %s', self.emotion_model_path)
             self.emotion_model = load_model(emotion_model_path)
-            print('Model ' + emotion_model_path + ' loaded successfully')
 
-            # the following two lines (and the with... line in predict_emotion)
-            # are necessary for avoiding
+            # the following two lines are necessary for avoiding
             # https://github.com/keras-team/keras/issues/2397
             self.emotion_model._make_predict_function()
             self.computation_graph = tf.get_default_graph()
         except:
-            print('Failed to load model ', emotion_model_path)
+            rospy.logerr('[recognize_emotion] Model %s could not be loaded', self.emotion_model_path)
+        return FTSMTransitions.INITIALISED
 
-    def execute(self, userdata):
-        number_of_faces = userdata.recognize_emotion_goal.number_of_faces
-        bounding_boxes = userdata.recognize_emotion_goal.bounding_boxes
-        userdata.emotions = list()
+    def running(self):
+        number_of_faces = self.goal.number_of_faces
+        bounding_boxes = self.goal.bounding_boxes
+        emotions = []
 
-        rgb_image = self.ros2cv(userdata.recognize_emotion_goal.image)
-        gray_image = self.rgb2gray(rgb_image)
+        rospy.loginfo('[recognize_emotion] Recognizing emotions')
+        rgb_image = self.__ros2cv(self.goal.image)
+        gray_image = self.__rgb2gray(rgb_image)
         for face in bounding_boxes:
             x, y, w, h = face.bounding_box_coordinates
             face = gray_image[y: (y + h), x: (x + w)]
             face = cv2.resize(face, self.image_size[0:2])
             face = np.expand_dims(face, 0)
             face = np.expand_dims(face, -1)
-            face = self.preprocess_image(face)
-            predicted_emotion = self.predict_emotion(face)
-            userdata.emotions.append(predicted_emotion)
+            face = self.__preprocess_image(face)
+            predicted_emotion = self.__predict_emotion(face)
+            emotions.append(predicted_emotion)
 
             rgb_cv2 = cv2.rectangle(rgb_image, (x, y), (x + w, y + h),
                                     (0, 0, 255), 2)
@@ -81,39 +67,31 @@ class RecognizeEmotion(smach.State):
                         1, cv2.LINE_AA)
         output_ros_image = self.bridge.cv2_to_imgmsg(rgb_image, 'bgr8')
         self.image_publisher.publish(output_ros_image)
-        return 'succeeded'
 
-    def preprocess_image(self, image):
+        self.result = self.set_result(True, emotions)
+        return FTSMTransitions.DONE
+
+    def set_result(self, success, emotions):
+        result = RecognizeEmotionResult()
+        result.success = success
+        result.emotions = emotions
+        return result
+
+    def __preprocess_image(self, image):
         image = image / 255.0
         return image
 
-    def predict_emotion(self, face):
+    def __predict_emotion(self, face):
         label = -1
         with self.computation_graph.as_default():
             class_predictions = self.emotion_model.predict(face)
             label = self.labels[np.argmax(class_predictions)]
         return label
 
-    def ros2cv(self, ros_image):
+    def __ros2cv(self, ros_image):
         cv_image = self.bridge.imgmsg_to_cv2(ros_image, 'bgr8')
         return np.array(cv_image, dtype=np.uint8)
 
-    def rgb2gray(self, image):
+    def __rgb2gray(self, image):
         gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         return gray_image
-
-
-class SetActionLibResult(smach.State):
-    def __init__(self, result):
-        smach.State.__init__(self, outcomes=['succeeded'],
-                             input_keys=['recognize_emotion_goal', 'emotions'],
-                             output_keys=['recognize_emotion_feedback',
-                                          'recognize_emotion_result'])
-        self.result = result
-
-    def execute(self, userdata):
-        result = RecognizeEmotionResult()
-        result.success = self.result
-        result.emotions = userdata.emotions
-        userdata.recognize_emotion_result = result
-        return 'succeeded'
