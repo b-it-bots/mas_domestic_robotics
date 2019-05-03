bee05a109bbcbb271054e8582ea346248974fda6
Patrick Nagel <patrick.nagel@smail.inf.h-brs.de>
2019-05-03 19:14:27 +0200

    Add the scenario states/ actions to the state machine of "where is this".
    This commit adds mainly dummy scenario states for the "where is this" scenario. Also those reference are defined in the state machine now. In the upcoming commits the logic within the states is going to be defined further.

---
+++ mdr_planning/mdr_scenarios/mdr_robocup_tasks/mdr_where_is_this/ros/src/mdr_where_is_this/scenario_states/give_directions.py
@@ -0,0 +1,16 @@
+from mas_execution_manager.scenario_state_base import ScenarioStateBase
+
+class GiveDirections(ScenarioStateBase):
+    def __init__(self, save_sm_state=False, **kwargs):
+        ScenarioStateBase.__init__(self, 'give_directions',
+                                   save_sm_state=save_sm_state,
+                                   outcomes=['succeeded', 'failed'])
+        self.sm_id = kwargs.get('sm_id', '')
+        self.state_name = kwargs.get('state_name', 'give_directions')
+        self.number_of_retries = kwargs.get('number_of_retries', 0)
+
+    def execute(self, userdata):
+        if self.save_sm_state:
+            self.save_current_state()
+
+        return 'succeeded'
