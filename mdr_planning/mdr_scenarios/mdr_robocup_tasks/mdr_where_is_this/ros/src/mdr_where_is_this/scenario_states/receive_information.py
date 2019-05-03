2a7c387aa1591f84c5505b0d883c24723bb9563e
Patrick Nagel <patrick.nagel@smail.inf.h-brs.de>
2019-05-03 19:24:30 +0200

    Add remaining states (without proper functioning yet).
    This commit adds the states of the scenario. At the moment all of them return succeeded so that the overall state machine can be tested.

--- mdr_planning/mdr_scenarios/mdr_robocup_tasks/mdr_where_is_this/ros/src/mdr_where_is_this/scenario_states/receive_information.py
+++ mdr_planning/mdr_scenarios/mdr_robocup_tasks/mdr_where_is_this/ros/src/mdr_where_is_this/scenario_states/receive_information.py
@@ -1,18 +1,18 @@
 from mas_execution_manager.scenario_state_base import ScenarioStateBase
 
-class GiveDirections(ScenarioStateBase):
+class ReceiveInformation(ScenarioStateBase):
     def __init__(self, save_sm_state=False, **kwargs):
-        ScenarioStateBase.__init__(self, 'give_directions',
+        ScenarioStateBase.__init__(self, 'receive_information',
                                    save_sm_state=save_sm_state,
                                    outcomes=['succeeded', 'failed'])
         self.sm_id = kwargs.get('sm_id', '')
-        self.state_name = kwargs.get('state_name', 'give_directions')
+        self.state_name = kwargs.get('state_name', 'receive_information')
         self.number_of_retries = kwargs.get('number_of_retries', 0)
 
     def execute(self, userdata):
         if self.save_sm_state:
             self.save_current_state()
 
-        self.say("Give Direction state.")
+        self.say("Receive information state.")
 
         return 'succeeded'
