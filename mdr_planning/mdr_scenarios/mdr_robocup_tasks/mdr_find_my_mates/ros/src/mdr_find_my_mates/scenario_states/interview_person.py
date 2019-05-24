690a2e7b750e724832af3ff459414aaaf5caf718
rkutty1 <jupey1994@gmail.com>
2019-05-24 16:50:48 +0200

    codacy fixes in interview_person.py

--- mdr_planning/mdr_scenarios/mdr_robocup_tasks/mdr_find_my_mates/ros/src/mdr_find_my_mates/scenario_states/interview_person.py
+++ mdr_planning/mdr_scenarios/mdr_robocup_tasks/mdr_find_my_mates/ros/src/mdr_find_my_mates/scenario_states/interview_person.py
@@ -6,10 +6,6 @@ from mas_perception_msgs.msg import Person
 from mdr_perception_msgs.msg import PersonInfo
 from mas_execution_manager.scenario_state_base import ScenarioStateBase
 
-from rasa_nlu.training_data import load_data
-from rasa_nlu import config
-from rasa_nlu.components import ComponentBuilder
-from rasa_nlu.model import Interpreter, Metadata, Trainer
 
 from mdr_listen_action.msg import ListenAction, ListenGoal
 
