7c0919524a223ad9bde54c3f6a2817b7b4231b0d
Lucy <robotics@mail.inf.h-brs.de>
2019-09-18 12:42:14 +0200

    update package info for elevator task

--- mdr_planning/mdr_scenarios/mdr_robocup_tasks/mdr_sciroc_elevator/setup.py
+++ mdr_planning/mdr_scenarios/mdr_robocup_tasks/mdr_sciroc_elevator/setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['mdr_where_is_this'],
+    packages=['mdr_sciroc_elevator'],
     package_dir={'': 'ros/src'}
 )
 
