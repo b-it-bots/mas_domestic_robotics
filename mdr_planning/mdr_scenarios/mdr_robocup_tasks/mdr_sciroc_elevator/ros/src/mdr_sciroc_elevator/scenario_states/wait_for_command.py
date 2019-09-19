6d45689174e6a17ef5d53ea154359004c7f96205
Lucy <robotics@mail.inf.h-brs.de>
2019-09-19 16:00:40 +0200

    Added datahub connection

--- mdr_planning/mdr_scenarios/mdr_robocup_tasks/mdr_sciroc_elevator/ros/src/mdr_sciroc_elevator/scenario_states/wait_for_command.py
+++ mdr_planning/mdr_scenarios/mdr_robocup_tasks/mdr_sciroc_elevator/ros/src/mdr_sciroc_elevator/scenario_states/wait_for_command.py
@@ -1,5 +1,6 @@
 import rospy
 from mas_execution_manager.scenario_state_base import ScenarioStateBase
+from sciroc_datahub_client.datahub_client import DataHubClient
 
 
 class WaitForCommand(ScenarioStateBase):
@@ -12,9 +13,13 @@ class WaitForCommand(ScenarioStateBase):
         self.state_name = kwargs.get('state_name', 'wait_for_command')
         self.number_of_retries = kwargs.get('number_of_retries', 0)
         self.timeout = kwargs.get('timeout', 25)
+        self.data_hub = DataHubClient.default_init()
 
     def execute(self, userdata):
         self.say('waiting for command from the data hub')
+        shop = self.data_hub.get_shop()
+        print("Shop ", shop)
+        userdata.floor = shop['floor']
         rospy.sleep(5.0)
         return 'succeeded'
 
