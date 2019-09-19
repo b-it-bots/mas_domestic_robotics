d2503bd7adcfcafdfb6d74d620cdb1e5dfa971e4
Lucy <robotics@mail.inf.h-brs.de>
2019-09-19 10:36:30 +0200

    Initial creation of the service for rasa

--- mdr_planning/mdr_scenarios/mdr_robocup_tasks/mdr_sciroc_elevator/ros/src/mdr_sciroc_elevator/scripts/rasa_interpreter_pub.py
+++ mdr_planning/mdr_scenarios/mdr_robocup_tasks/mdr_sciroc_elevator/ros/src/mdr_sciroc_elevator/scripts/rasa_interpreter_pub.py
@@ -2,10 +2,14 @@
 
 from rasa.nlu.model import Interpreter 
 import rospy 
+from mdr_sciroc_elevator.srv import rasa_interpreter 
 
-def rasa_pub():
-    model_path = "/home/lucy/ros/kinetic/src/mas_models/rasa_nlu_models/common/sciroc/elevator"
-    interpreter = Interpreter(model_path)
-
+class RasaNLU():
+    def __init__(self,path):
+        # Loads Rasa NLU 
+        self.interpreter = Interpreter.load(path)
+        
 if __name__ == "__main__":
-    rasa_pub()
+    model_path = "/home/lucy/ros/kinetic/src/mas_models/rasa_nlu_models/common/sciroc/elevator/nlu"
+    rospy.init_node("nlu_model", anonymous=True)
+    rasa = RasaNLU(model_path)
