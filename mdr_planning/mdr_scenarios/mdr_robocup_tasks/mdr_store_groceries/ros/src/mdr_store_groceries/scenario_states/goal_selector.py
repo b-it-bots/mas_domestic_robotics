fedb259e1d99fee173d6d1c4a930a2432ec1ead9
Alex Mitrevski <aleksandar.mitrevski@h-brs.de>
2020-01-26 15:30:13 +0100

    [store_groceries/goal_selector] Modifications to handle the updated simplified domain

--- mdr_planning/mdr_scenarios/mdr_robocup_tasks/mdr_store_groceries/ros/src/mdr_store_groceries/scenario_states/goal_selector.py
+++ mdr_planning/mdr_scenarios/mdr_robocup_tasks/mdr_store_groceries/ros/src/mdr_store_groceries/scenario_states/goal_selector.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from mas_execution_manager.scenario_state_base import ScenarioStateBase
 from mdr_rosplan_interface.planner_interface import PlannerInterface
 
@@ -28,10 +30,6 @@ class StoreGroceriesGoalSelector(ScenarioStateBase):
 
             scanning_goals = self.get_scanning_goals()
             self.planner_interface.remove_plan_goals(scanning_goals)
-
-            # we ensure that the storing locations of the
-            # objects are correctly set before creating a plan
-            self.set_storing_locations()
 
             storing_goals = self.get_storing_groceries_goals()
             self.planner_interface.add_plan_goals(storing_goals)
@@ -70,27 +68,61 @@ class StoreGroceriesGoalSelector(ScenarioStateBase):
             goals.append(('explored', [('plane', 'shelf{0}'.format(i+1))]))
         return goals
 
-    def set_storing_locations(self):
-        '''Adds a list of "stored_on" predicates to the knowledge base, which
-        specify which category of objects is stored on which shelf. This information
-        is obtained by retriving the categories of the objects seen on
-        the individual shelves.
+    def get_storing_groceries_goals(self):
+        '''Adds a list of "stored_on" predicates to the knowledge base that indicate
+        which category of objects is stored on which shelf and returns a list of
+        "on" predicates indicating where each object should be stored.
         '''
         obj_category_map = self.planner_interface.kb_interface.get_obj_category_map()
+        category_storage_locations = self.get_category_storing_locations(obj_category_map)
+
+        # we add predicates about the storage locations of objects to the knowledge base
         storage_fact_list = []
+        for category, location in category_storage_locations.items():
+            storage_fact_list.append(('stored_on', [('class', category),
+                                                    ('plane', location)]))
+        self.planner_interface.kb_interface.insert_facts(storage_fact_list)
+
+        # we create a list of storage goals - an "on" predicate per object that needs to be stored
+        goals = []
+        for obj, obj_category in obj_category_map.items():
+            goals.append(('on', [('obj', obj),
+                                 ('plane', category_storage_locations[obj_category])]))
+        return goals
+
+    def get_category_storing_locations(self, obj_category_map):
+        '''Returns a dictionary in which each key is an object category and the
+        value specifies which shelf that object category is stored on.
+        This information is obtained by retriving the categories of
+        the objects seen on the individual shelves.
+
+        The storage locations of object categories that haven't been observed
+        on the shelves are assigned randomly.
+        '''
+        # we keep track of the categories whose locations have been assigned
+        # so that we can check if there are categories for which the storage
+        # location is not known; if there are such categories, we select a
+        # storage location from the available shelves at random
+        assigned_categories = set()
+
+        category_storage_locations = dict()
         for i in range(self.number_of_shelves):
             shelf_name = 'shelf{0}'.format(i+1)
             shelf_object_names = self.planner_interface.kb_interface.get_surface_object_names(shelf_name)
             for obj in shelf_object_names:
                 # TODO: what to do if the recognition fails and some category
                 # of objects seems to be stored on multiple shelves?
-                storage_fact_list.append(('stored_on', [('class', obj_category_map[obj]),
-                                                        ('plane', shelf_name)]))
-        self.planner_interface.kb_interface.insert_facts(storage_fact_list)
+                category_storage_locations[obj_category_map[obj]] = shelf_name
+                assigned_categories.add(obj_category_map[obj])
 
-    def get_storing_groceries_goals(self):
-        '''Returns a list containing a single planning goal -
-        a "groceries_stored" predicate.
-        '''
-        goals = [('groceries_stored')]
-        return goals
+        unassigned_categories = set([cat for obj, cat in obj_category_map.items()]) - assigned_categories
+        if unassigned_categories:
+            print('Storage locations of {0} unknown'.format(unassigned_categories))
+            for category in unassigned_categories:
+                shelf_idx = np.random.randint(1, self.number_of_shelves+1)
+                shelf_name = 'shelf{0}'.format(shelf_idx)
+                print('Setting storage location of "{0}" to "{1}"'.format(category,
+                                                                          shelf_name))
+                category_storage_locations[category] = shelf_name
+
+        return category_storage_locations
