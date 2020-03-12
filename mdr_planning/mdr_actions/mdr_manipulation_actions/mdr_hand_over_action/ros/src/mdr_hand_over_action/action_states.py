9acbea23e23e81d6ec5f3f4dea742c241805b38d
AhmedFaisal95 <ahmedfaisal_95@hotmail.co.uk>
2020-03-12 16:59:06 +0100

    [hand-over-action] Add boolean release detection parameter, and some code clean-up

--- mdr_planning/mdr_actions/mdr_manipulation_actions/mdr_hand_over_action/ros/src/mdr_hand_over_action/action_states.py
+++ mdr_planning/mdr_actions/mdr_manipulation_actions/mdr_hand_over_action/ros/src/mdr_hand_over_action/action_states.py
@@ -26,7 +26,7 @@ class HandOverSM(ActionSMBase):
                  move_arm_server='move_arm_server',
                  init_config_name = 'neutral',
                  hand_over_policy_config_dir='',
-                 hand_over_position_policy_parameters_file = '800_py2.pkl',
+                 hand_over_position_policy_parameters_file = 'learned_position_policy_parameters.pkl',
                  hand_over_dmp_weights_dir='',
                  hand_over_dmp = 'grasp.yaml',
                  dmp_tau = 30.,
@@ -47,18 +47,14 @@ class HandOverSM(ActionSMBase):
 
         self.hand_over_dmp = hand_over_dmp
         self.dmp_tau = dmp_tau
-        # self.hand_over_position_policy_parameters_file = hand_over_position_policy_parameters_file
-        # self.hand_over_position_policy_parameters_file = 'contextHOs_160iters-Sigma_restarts_at_10s, eps:1, N:40, Con:x-diag, Goals:(Se:[0.5, 0.078, 0.7], Ly:[0.7, 0.078, 0.7], St:[0.4, 0.078, 1.0]).pkl'
-        self.hand_over_position_policy_parameters_file = join(self.hand_over_policy_config_dir, '160_py2.pkl')
+        self.hand_over_position_policy_parameters_file = join(self.hand_over_policy_config_dir, hand_over_position_policy_parameters_file)
         self.hand_over_position_policy_parameters = self.load_policy_params_from_file(self.hand_over_position_policy_parameters_file)
 
         self.tf_listener = tf.TransformListener()
         self.force_sensor_topic = force_sensor_topic
 
         self.latest_force_measurement_x = 0.
-        self.latest_force_measurement_z = 0.
         self.cumsum_x = 0
-        self.cumsum_z = 0
         self.force_detection_threshold = 1.
         self.object_reception_detected = False
 
@@ -74,23 +70,19 @@ class HandOverSM(ActionSMBase):
         return FTSMTransitions.INITIALISED
 
     def running(self):
-        posture_type = self.goal.posture_type
-        obstacle = self.goal.obstacle
-        context_aware = self.goal.context_aware
-
         hand_over_pose = PoseStamped()
         hand_over_pose.header.frame_id = 'base_link'
         hand_over_pose.header.stamp = rospy.Time.now()
-        if context_aware:
+        if self.goal.context_aware:
             # > Pick context-dependent hand-over position:
             policy_parameter_a = self.hand_over_position_policy_parameters[0][0]
             policy_parameter_A = self.hand_over_position_policy_parameters[0][1]
 
-            if posture_type == 'standing':
+            if self.goal.posture_type == 'standing':
                 context_vector = np.array([0.0, 0.0, 0.4])
-            elif posture_type == 'seated':
+            elif self.goal.posture_type == 'seated':
                 context_vector = np.array([0.5, 0.0, 0.0])
-            elif posture_type == 'lying':
+            elif self.goal.posture_type == 'lying':
                 context_vector = np.array([0.0, 0.7, 0.0])
         
             # Sample upper-level policy (with no exploration) for hand-over position;
@@ -114,87 +106,60 @@ class HandOverSM(ActionSMBase):
         pose_base_link = self.tf_listener.transformPose('base_link', hand_over_pose)
 
         # > Pick context_dependent hand-over trajectory shape:
-        if not obstacle:
+        if not self.goal.obstacle:
             trajectory_weights_filename = 'grasp.yaml'
         else:
+            ## Uncomment one of the following if testing other learned parameters is desired:
+            
             ## Initial learned trajectory:
             # trajectory_weights_filename = 'learned_obstacle_avoiding_weights.yaml'
             
-            ## Smoothed trajectories:
-            # trajectory_weights_filename = 'learned_smoothed_obstacle_avoiding_weights_2900iters.yaml'
-            # trajectory_weights_filename = 'learned_smoothed_obstacle_avoiding_weights_2100iters.yaml'
-            # trajectory_weights_filename = 'learned_smoothed_obstacle_avoiding_weights_1600iters.yaml'
-            # trajectory_weights_filename = 'learned_smoothed_obstacle_avoiding_weights_800iters.yaml'
+            ## Smoothed learned trajectory:
+            # trajectory_weights_filename = 'learned_smoothed_obstacle_avoiding_weights.yaml'
 
-            ## Smoothed trajectories, with corrected end_points:
-            # trajectory_weights_filename = 'learned_smoothed_corrected_obstacle_avoiding_weights_2900iters.yaml'
-            trajectory_weights_filename = 'learned_smoothed_corrected_obstacle_avoiding_weights_2100iters.yaml'
-            # trajectory_weights_filename = 'learned_smoothed_corrected_obstacle_avoiding_weights_1600iters.yaml'
-            # trajectory_weights_filename = 'learned_smoothed_corrected_obstacle_avoiding_weights_800iters.yaml'
+            ## Learned underhand trajectory:
+            # trajectory_weights_filename = 'learned_smoothed_corrected_underhand_weights.yaml'
 
-            ## Underhand trajectories:
-            # trajectory_weights_filename = 'learned_smoothed_corrected_underhand_weights_7700iters.yaml'
-            # trajectory_weights_filename = 'learned_smoothed_corrected_underhand_weights_6900iters.yaml'
-            # trajectory_weights_filename = 'learned_smoothed_corrected_underhand_weights_6100iters.yaml'
-            # trajectory_weights_filename = 'learned_smoothed_corrected_underhand_weights_5300iters.yaml'
-            # trajectory_weights_filename = 'learned_smoothed_corrected_underhand_weights_4500iters.yaml'
-            # trajectory_weights_filename = 'learned_smoothed_corrected_underhand_weights_3700iters.yaml'
-            # trajectory_weights_filename = 'learned_smoothed_corrected_underhand_weights_2900iters.yaml'
-            # trajectory_weights_filename = 'learned_smoothed_corrected_underhand_weights_2100iters.yaml'
-            # trajectory_weights_filename = 'learned_smoothed_corrected_underhand_weights_1600iters.yaml'
-            # trajectory_weights_filename = 'learned_smoothed_corrected_underhand_weights_800iters.yaml'
-
+            ## Smoothed learned trajectory, with corrected end_points:
+            trajectory_weights_filename = 'learned_smoothed_corrected_obstacle_avoiding_weights.yaml'
 
         self.hand_over_dmp = join(self.hand_over_dmp_weights_dir, trajectory_weights_filename)
 
         ## TODO: determine whether aligning base is necessary for hand_over action here:
         ## ...
 
-        # # Go to pregrasp low position:
-        # rospy.loginfo('[hand_over] Moving back to pregrasp low position...')
-        # self.__move_arm(MoveArmGoal.NAMED_TARGET, 'pregrasp_low')
-
-        # # Initialize by grasping a provided object:
-        # rospy.loginfo('[hand_over] Closing the gripper')
-        # self.gripper.close() 
-
         # Move to chosen hand_over position, along appropriate trajectory:
         rospy.loginfo('[hand_over] Handing object over...')
         self.__move_arm(MoveArmGoal.END_EFFECTOR_POSE, pose_base_link)
 
-        # Naive object release strategy:
-        # # --------------------------------------------------------------
-        # # Wait for person to hold object:
-        # rospy.loginfo('[hand_over] Waiting before releasing object...')
-        # rospy.sleep(5.)
+        if not release_detection:
+            # Naive object release strategy:
+            rospy.loginfo('[hand_over] Waiting before releasing object...')
+            rospy.sleep(5.)
 
-        # # Release the object:
-        # rospy.loginfo('[hand_over] Opening the gripper...')
-        # self.gripper.open()
-        # #  --------------------------------------------------------------
-
-        # Force sensing object release strategy:
-        # --------------------------------------------------------------
-        # Wait for person to pull object:
-        rospy.sleep(1.)
-        rospy.loginfo('[hand_over] Waiting for object to be received...')
-        rospy.Subscriber(self.force_sensor_topic, WrenchStamped, self.force_sensor_cb)
-        self.object_reception_detected = False
-        self.cumsum_x = 0.
-        self.cumsum_z = 0.
-
-        start_time = rospy.get_time()
-        while (rospy.get_time() - start_time) < 50 and not self.object_reception_detected:
-            self.detect_object_reception()
-            rospy.sleep(0.1)
-
-        # If a pull is detected, release the object:
-        if self.object_reception_detected:        
-            rospy.loginfo('[hand_over] Opening the gripper to release object...')
+            # Release the object:
+            rospy.loginfo('[hand_over] Opening the gripper...')
             self.gripper.open()
+
         else:
-            rospy.loginfo('[hand_over] Keeping object since no reception was detected')
-        # --------------------------------------------------------------        
+            # Force sensing object release strategy:
+            rospy.sleep(1.)
+            rospy.loginfo('[hand_over] Waiting for object to be received...')
+            rospy.Subscriber(self.force_sensor_topic, WrenchStamped, self.force_sensor_cb)
+            self.object_reception_detected = False
+            self.cumsum_x = 0.
+
+            start_time = rospy.get_time()
+            while (rospy.get_time() - start_time) < 50 and not self.object_reception_detected:
+                self.detect_object_reception()
+                rospy.sleep(0.1)
+
+            # If a pull is detected, release the object:
+            if self.object_reception_detected:        
+                rospy.loginfo('[hand_over] Opening the gripper to release object...')
+                self.gripper.open()
+            else:
+                rospy.loginfo('[hand_over] Keeping object since no reception was detected')
 
         # Return to a neutral arm position:
         rospy.loginfo('[hand_over] Moving back to neutral position...')
@@ -250,23 +215,11 @@ class HandOverSM(ActionSMBase):
         pdf_1 = norm(mu_1, std)
 
         self.cumsum_x += max(0, np.log(pdf_1.pdf(self.latest_force_measurement_x) / pdf_0.pdf(self.latest_force_measurement_x)))
-        self.cumsum_z += max(0, np.log(pdf_1.pdf(self.latest_force_measurement_z) / pdf_0.pdf(self.latest_force_measurement_z)))
 
         print(self.cumsum_x)
         if self.cumsum_x > self.force_detection_threshold:
-        # if self.cumsum_x > self.force_detection_threshold or self.cumsum_z > self.force_detection_threshold:
             rospy.loginfo('[hand_over] Object reception detected!')
             self.object_reception_detected = True
         
     def force_sensor_cb(self, force_sensor_msg):
         self.latest_force_measurement_x = force_sensor_msg.wrench.force.x
-        self.latest_force_measurement_z = force_sensor_msg.wrench.force.z
-
-        # print('[hand_over DEBUG] Force Measurements:')
-        # print('[hand_over DEBUG] Current cumsum in x:', self.cumsum_x)
-        # print('[hand_over DEBUG] Current cumsum in z:', self.cumsum_z)
-        # print("*********************")
-        # print('[hand_over DEBUG] Current Force Measurements, x:', force_sensor_msg.wrench.force.x)
-        # print('[hand_over DEBUG] Current Force Measurements, y:', force_sensor_msg.wrench.force.y)
-        # print('[hand_over DEBUG] Current Force Measurements, z:', force_sensor_msg.wrench.force.z)
-        # print("\n\n")
