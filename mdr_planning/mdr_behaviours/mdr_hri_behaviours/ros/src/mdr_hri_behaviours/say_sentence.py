2ca1b9349a5221887966c00499d4b2997145f275
AhmedFaisal95 <ahmedfaisal_95@hotmail.co.uk>
2020-07-04 16:55:37 +0200

    Add simple say sentence HRI behaviour

---
+++ mdr_planning/mdr_behaviours/mdr_hri_behaviours/ros/src/mdr_hri_behaviours/say_sentence.py
@@ -0,0 +1,22 @@
+import time
+import rospy
+import random
+
+from mas_execution_manager.scenario_state_base import ScenarioStateBase
+
+class SaySentence(ScenarioStateBase):
+    def __init__(self, save_sm_state=False, **kwargs):
+        ScenarioStateBase.__init__(self, 'say_sentence',
+                                   save_sm_state=save_sm_state,
+                                   outcomes=['succeeded'])
+        self.sm_id = kwargs.get('sm_id', '')
+        self.state_name = kwargs.get('state_name', 'say_something')
+        self.sentences = list(kwargs.get('sentences', list()))
+
+    def execute(self, userdata):
+        sentence = random.choice(self.sentences)
+
+        rospy.loginfo('Saying: %s' % sentence)
+        self.say(sentence)
+
+        return 'succeeded'
