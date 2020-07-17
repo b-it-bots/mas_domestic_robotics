b8b89bda8fb052e308b7a0fa53c426f54ad10e60
Alex Mitrevski <aleksandar.mitrevski@h-brs.de>
2020-07-17 16:31:51 +0200

    [demo_lab_manager] Added stub verify_person implementation

---
+++ mdr_planning/mdr_scenarios/mdr_demos/mdr_demo_lab_manager/ros/src/scenario_states/verify_person.py
@@ -0,0 +1,24 @@
+import rospy
+from mas_execution_manager.scenario_state_base import ScenarioStateBase
+
+class VerifyPerson(ScenarioStateBase):
+    def __init__(self, save_sm_state=False, **kwargs):
+        ScenarioStateBase.__init__(self, 'verify_person',
+                                   save_sm_state=save_sm_state,
+                                   outcomes=['no_empty_spot', 'new_person',
+                                             'already_logged_person',
+                                             'known_person'])
+        self.sm_id = kwargs.get('sm_id', '')
+        self.state_name = kwargs.get('state_name', 'verify_person')
+        self.number_of_retries = kwargs.get('number_of_retries', 0)
+        self.total_locations = kwargs.get('total_locations', 0)
+        self.retry_count = 0
+        self.timeout = 120.
+
+    def execute(self, userdata):
+        rospy.loginfo('[verify_person] Verifying person')
+
+        occupied_spots = self.kb_interface.get_all_attributes('occupied_location')
+        if len(occupied_spots) == self.total_locations:
+            return 'no_empty_spot'
+        return 'new_person'
