4af591d9139eae7e60dc11eee99e7c926d5c248b
Lucy <robotics@mail.inf.h-brs.de>
2020-07-25 15:54:35 +0200

    [demo_lab_manager/scenario_states/verify_person] Adding the occupancy checking implemented by Dharmin yesterday

--- mdr_planning/mdr_scenarios/mdr_demos/mdr_demo_lab_manager/ros/src/mdr_demo_lab_manager/scenario_states/verify_person.py
+++ mdr_planning/mdr_scenarios/mdr_demos/mdr_demo_lab_manager/ros/src/mdr_demo_lab_manager/scenario_states/verify_person.py
@@ -2,6 +2,8 @@ import random
 
 import rospy
 from mas_execution_manager.scenario_state_base import ScenarioStateBase
+from rosplan_knowledge_msgs.msg import DomainFormula
+from diagnostic_msgs.msg import KeyValue
 
 class VerifyPerson(ScenarioStateBase):
     def __init__(self, save_sm_state=False, **kwargs):
@@ -17,14 +19,23 @@ class VerifyPerson(ScenarioStateBase):
         self.total_locations = kwargs.get('total_locations', 0)
         self.retry_count = 0
         self.timeout = 120.
+        occupied_locations = self.kb_interface.get_obj_instance('occupied_locations', DomainFormula._type)
+        if occupied_locations is None:
+            self.kb_interface.insert_obj_instance('occupied_locations', DomainFormula())
 
     def execute(self, userdata):
         rospy.loginfo('[verify_person] Verifying person')
 
-        occupied_spots = self.kb_interface.get_all_attributes('occupied_location')
-        if len(occupied_spots) == self.total_locations:
+        occupied_locations = self.kb_interface.get_obj_instance('occupied_locations', DomainFormula._type)
+        occ_spots = [kv.key for kv in occupied_locations.typed_parameters]
+        if len(occupied_locations.typed_parameters) == self.total_locations:
             return 'no_empty_spot'
 
-        spot = random.randint(1, self.total_locations)
+        spots = [str(i+1) for i in range(self.total_locations)]
+        for occ_spot in occ_spots:
+            spots.remove(occ_spot)
+        spot = random.choice(spots)
         userdata.destination_locations = ['spot_{0}'.format(spot)]
+        occupied_locations.typed_parameters.append(KeyValue(key=str(spot), value='true'))
+        self.kb_interface.update_obj_instance('occupied_locations', occupied_locations)
         return 'new_person'
