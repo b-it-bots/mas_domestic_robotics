9f96e0c6e8629b0ae44b0c5be3e3dbd617d48892
Alex Mitrevski <aleksandar.mitrevski@h-brs.de>
2020-08-16 18:52:55 +0200

    [demo_lab_manager/verify_person] Refactored to use the 'recognise_person' fn from the KB interface
    The function was added with
    https://github.com/b-it-bots/mas_knowledge_base/pull/39

--- mdr_planning/mdr_scenarios/mdr_demos/mdr_demo_lab_manager/ros/src/mdr_demo_lab_manager/scenario_states/verify_person.py
+++ mdr_planning/mdr_scenarios/mdr_demos/mdr_demo_lab_manager/ros/src/mdr_demo_lab_manager/scenario_states/verify_person.py
@@ -24,7 +24,7 @@ class VerifyPerson(ScenarioStateBase):
         self.state_name = kwargs.get('state_name', 'verify_person')
         self.number_of_retries = kwargs.get('number_of_retries', 0)
         self.total_locations = kwargs.get('total_locations', 0)
-        self.threshold = kwargs.get('threshold', 0)
+        self.person_recognition_threshold = kwargs.get('person_recognition_threshold', 0)
         self.retry_count = 0
         self.timeout = kwargs.get('timeout', 10.)
         occupied_locations = self.kb_interface.get_obj_instance('occupied_locations', DomainFormula._type)
@@ -42,44 +42,36 @@ class VerifyPerson(ScenarioStateBase):
         rospy.loginfo('[verify_person] Verifying person')
 
         # Look for occupied spots
-        occupied_locations = self.kb_interface.get_obj_instance('occupied_locations', DomainFormula._type)
+        occupied_locations = self.kb_interface.get_obj_instance('occupied_locations',
+                                                                DomainFormula._type)
         occ_spots = [kv.key for kv in occupied_locations.typed_parameters]
         if len(occupied_locations.typed_parameters) == self.total_locations:
             return 'no_empty_spot'
 
         # Try to match face to current people in the lab
-        known_people = self.kb_interface.get_obj_of_type(Person._type)
-        unknown_person = self.kb_interface.get_obj_instance('person_0', Person._type)
+        recognised_person = self.kb_interface.recognise_person('person_0',
+                                                               Person._type,
+                                                               self.person_recognition_threshold)
+        if recognised_person is not None:
+            self.say("Hello {0}".format(recognised_person.name))
+            self.say("If you would like to free up your spot, please say goodbye.")
 
-        unknown_person_embedding = np.array(unknown_person.face.views[0].embedding.embedding)
-        for known_person in known_people:
-            distance = np.linalg.norm(np.array(known_person.face.views[0].embedding.embedding) - \
-                                      unknown_person_embedding)
+            # wait for goodbye
+            goal = ListenGoal()
+            self.listen_client.send_goal(goal)
+            self.listen_client.wait_for_result(rospy.Duration.from_sec(int(self.timeout)))
+            listen_state = self.listen_client.get_state()
+            listen_result = self.listen_client.get_result()
 
-            if np.linalg.norm(distance) < self.threshold:
-                self.say("Hello {0}".format(known_person.name))
-                self.say("If you would like to free up your spot, please say goodbye.")
-                # wait for goodbye
-                goal = ListenGoal()
+            if listen_state == GoalStatus.SUCCEEDED and "bye" in listen_result.message:
+                self.say("Goodbye! {0}. stay safe!".format(recognised_person.name))
+                for occ_spot in occupied_locations.typed_parameters:
+                    if occ_spot.value == recognised_person.name:
+                        occupied_locations.typed_parameters.remove(occ_spot)
+                self.kb_interface.update_obj_instance('occupied_locations', occupied_locations)
 
-                self.listen_client.send_goal(goal)
-                self.listen_client.wait_for_result(rospy.Duration.from_sec(int(self.timeout)))
-                listen_state = self.listen_client.get_state()
-                listen_result = self.listen_client.get_result()
-
-                if listen_state == GoalStatus.SUCCEEDED and "bye" in listen_result.message:
-                    self.say("Goodbye! {0}. stay safe!".format(known_person.name))
-                    spot = known_person
-
-                    for occ_spot in occupied_locations.typed_parameters:
-                        if occ_spot.value == known_person.name:
-                            occupied_locations.typed_parameters.remove(occ_spot)
-
-                    self.kb_interface.update_obj_instance('occupied_locations', occupied_locations)
-                    self.kb_interface.remove_obj_instance(known_person.name, Person._type)
-
-                # otherwise return to monitor door
-                return 'known_person'
+            # otherwise return to monitor door
+            return 'known_person'
 
         # No matching face, treat as new person
         spots = [str(i+1) for i in range(self.total_locations)]
