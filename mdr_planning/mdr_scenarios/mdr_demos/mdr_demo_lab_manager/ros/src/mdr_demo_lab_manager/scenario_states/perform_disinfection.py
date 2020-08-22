e381ee1888160b8ef13cf466ba0f351efdf872e6
Alex Mitrevski <aleksandar.mitrevski@h-brs.de>
2020-08-22 16:45:40 +0200

    [demo_lab_manager] Added a state for sending a disinfection cmd

---
+++ mdr_planning/mdr_scenarios/mdr_demos/mdr_demo_lab_manager/ros/src/mdr_demo_lab_manager/scenario_states/perform_disinfection.py
@@ -0,0 +1,25 @@
+import rospy
+from std_msgs.msg import Empty
+from mas_execution_manager.scenario_state_base import ScenarioStateBase
+
+class PerformDisinfection(ScenarioStateBase):
+    def __init__(self, save_sm_state=False, **kwargs):
+        ScenarioStateBase.__init__(self, 'perform_disinfection',
+                                   save_sm_state=save_sm_state,
+                                   outcomes=['succeeded'])
+        self.sm_id = kwargs.get('sm_id', '')
+        self.state_name = kwargs.get('state_name', 'perform_disinfection')
+        self.disinfection_cmd_topic = kwargs.get('disinfection_cmd_topic',
+                                                 '/disinfect_cmd')
+        self.disinfection_duration = kwargs.get('disinfection_duration', 20.)
+
+        self.disinfection_cmd_pub = None
+        try:
+            self.disinfection_cmd_pub = rospy.Publisher(self.disinfection_cmd_topic,
+                                                        Empty, queue_size=1)
+        except:
+            rospy.logerr('[send_disinfection_command] Could not create a disinfection command publisher')
+
+    def execute(self, userdata):
+        self.disinfection_cmd_pub.publish(Empty())
+        rospy.sleep(self.disinfection_duration)
