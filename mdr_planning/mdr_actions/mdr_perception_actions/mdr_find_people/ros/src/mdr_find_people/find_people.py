800f6195e9b1c4d6fb8fbe22088b2ff56b372fb2
Alex Mitrevski <aleksandar.mitrevski@h-brs.de>
2020-08-22 13:45:50 +0200

    [find_people] Fixed import for draw_labeled_boxes (from mas_perception_libs)

--- mdr_planning/mdr_actions/mdr_perception_actions/mdr_find_people/ros/src/mdr_find_people/find_people.py
+++ mdr_planning/mdr_actions/mdr_perception_actions/mdr_find_people/ros/src/mdr_find_people/find_people.py
@@ -9,7 +9,8 @@ from std_msgs.msg import Header
 from geometry_msgs.msg import Pose, PoseStamped, Point, Quaternion
 from mas_perception_libs import ImageDetectionKey, ImageDetectorBase
 from mas_perception_libs.utils import cloud_msg_to_image_msg, cloud_msg_to_cv_image, \
-                                      crop_cloud_to_xyz, draw_labeled_boxes
+                                      crop_cloud_to_xyz
+from mas_perception_libs.visualization import draw_labeled_boxes
 from ssd_keras_ros import SSDKerasObjectDetector
 
 
