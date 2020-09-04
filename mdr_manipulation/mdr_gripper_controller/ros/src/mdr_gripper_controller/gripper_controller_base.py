49455d51082552acbd6da1ac5327eb1201ada78e
Alex Mitrevski <aleksandar.mitrevski@h-brs.de>
2020-09-04 17:49:11 +0200

    [gripper_controller] Added fns for impact detection along z (useful for placing)
    I want to do impact detection as change detection over force measurements,
    so I've added two functions:
    * one for initialising the measurements
    * one for checking if the difference between the initial measurements
      and the current measurements is above a threshold

--- mdr_manipulation/mdr_gripper_controller/ros/src/mdr_gripper_controller/gripper_controller_base.py
+++ mdr_manipulation/mdr_gripper_controller/ros/src/mdr_gripper_controller/gripper_controller_base.py
@@ -2,17 +2,25 @@ import rospy
 
 class GripperControllerBase(object):
     def open(self):
-        rospy.loginfo('[OPEN_GRIPPER] Ignoring request')
+        rospy.loginfo('[open] Ignoring request')
         raise NotImplementedError()
 
     def close(self):
-        rospy.loginfo('[CLOSE_GRIPPER] Ignoring request')
+        rospy.loginfo('[close] Ignoring request')
+        raise NotImplementedError()
+
+    def init_impact_detection_z(self):
+        rospy.loginfo('[init_impact_detection_z] Ignoring request')
+        raise NotImplementedError()
+
+    def detect_impact_z(self):
+        rospy.loginfo('[detect_impact_z] Ignoring request')
         raise NotImplementedError()
 
     def init_grasp_verification(self):
-        rospy.loginfo('[INIT_GRASP_VERIFICATION] Ignoring request')
+        rospy.loginfo('[init_grasp_verification] Ignoring request')
         raise NotImplementedError()
 
     def verify_grasp(self):
-        rospy.loginfo('[VERIFY_GRASP] Ignoring request')
+        rospy.loginfo('[verify_grasp] Ignoring request')
         raise NotImplementedError()
