793b07ee218fde6daf2b5fa83ee0cfe0fc009d02
AhmedFaisal95 <ahmedfaisal_95@hotmail.co.uk>
2020-09-18 20:14:34 +0200

    Add point cloud filtering to improve person pose estimation

--- mdr_planning/mdr_actions/mdr_perception_actions/mdr_find_people/ros/src/mdr_find_people/find_people.py
+++ mdr_planning/mdr_actions/mdr_perception_actions/mdr_find_people/ros/src/mdr_find_people/find_people.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python
 
 import os
+
 import numpy as np
+import pcl
+import face_recognition
+from sklearn.impute import SimpleImputer
 
 import rospy
 from rospkg import RosPack
-import face_recognition
-
 from std_msgs.msg import Header
 from geometry_msgs.msg import Pose, PoseStamped, Point, Quaternion
+
 from mas_perception_libs import ImageDetectionKey, ImageDetectorBase
 from mas_perception_libs.utils import cloud_msg_to_image_msg, cloud_msg_to_cv_image, \
-                                      crop_cloud_to_xyz
+                                      crop_cloud_to_xyz, crop_organized_cloud_msg
 from mas_perception_libs.visualization import draw_labeled_boxes
 from ssd_keras_ros import SSDKerasObjectDetector
 
@@ -67,8 +70,26 @@ class FindPeople(object):
         for i, _ in enumerate(predictions):
             bb2d = bounding_boxes[i]
 
-            cropped_cloud = crop_cloud_to_xyz(cloud_msg, bb2d)
-            mean_coords = np.nanmean(np.reshape(cropped_cloud, (-1, 3)), axis=0)
+            cropped_cloud = crop_organized_cloud_msg(cloud_msg, bb2d)
+            obj_coords = crop_cloud_to_xyz(cloud_msg, bb2d)
+
+            imputer = SimpleImputer(missing_values=np.nan, strategy='most_frequent')
+            imputer.fit(obj_coords.reshape(obj_coords.shape[0]*obj_coords.shape[1], obj_coords.shape[2]))
+            obj_coords_without_nans = imputer.transform(obj_coords.reshape(obj_coords.shape[0]*obj_coords.shape[1], obj_coords.shape[2]))
+
+            ## PCL Outlier Removal:
+            cloud_without_nans = pcl.PointCloud(obj_coords_without_nans.astype(np.float32))
+            voxel_grid_filter = cloud_without_nans.make_voxel_grid_filter()
+            voxel_grid_filter.set_leaf_size(0.01, 0.01, 0.01)
+            cloud_filtered = voxel_grid_filter.filter()
+
+            passthrough_filter = cloud_filtered.make_passthrough_filter()
+            passthrough_filter.set_filter_field_name("z")
+            passthrough_filter.set_filter_limits(0., 2.5)
+            cloud_filtered = passthrough_filter.filter()
+            cloud_filtered_array = cloud_filtered.to_array()
+
+            mean_coords = np.nanmean(cloud_filtered_array, axis=0)
             mean_ps = PoseStamped()
             mean_ps.header = Header(frame_id=cloud_msg.header.frame_id)
             mean_ps.pose = Pose(Point(mean_coords[0], mean_coords[1], mean_coords[2]), Quaternion())
