e9028adef6bb13fc2603dca851f962f532417432
AhmedFaisal95 <ahmedfaisal_95@hotmail.co.uk>
2020-11-14 09:49:41 +0100

    Add video device (camera or video file) in gesture classifier demo script

--- mdr_planning/mdr_actions/mdr_perception_actions/mdr_recognize_gesture_action/scripts/gesture_classifier_demo.py
+++ mdr_planning/mdr_actions/mdr_perception_actions/mdr_recognize_gesture_action/scripts/gesture_classifier_demo.py
@@ -1,19 +1,25 @@
 from __future__ import print_function
+import argparse
 
 import cv2
 import numpy as np
+
 from mdr_recognize_gesture.gesture_classifier import GestureClassifier
 
 
-float_formatter = "{:.3f}".format
-np.set_printoptions(formatter={'float_kind': float_formatter})
-
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--video_device', '-v', type=str,
+                        default='0', help='The video device. For a camera,'
+                        'provide its id, for e.g. 0. For a video file, provide'
+                        'its path')
+    args = parser.parse_args()
+
     gesture_classifier = GestureClassifier(gesture_distance_threshold=50., 
                                            model_path='/home/afaisal/.models/openpose_models/',
                                            gesture_types=['waving', 'nodding', 'shaking_head', 'go_away', 'come_closer', 'pointing'],
                                            known_example_data_dir='../data/gesture_examples/')
-    gesture_data = gesture_classifier.capture_gesture_data()
+    gesture_data = gesture_classifier.capture_gesture_data(args.video_device)
     most_likely_class = gesture_classifier.classify_gesture(gesture_data)
 
     if most_likely_class is None:
