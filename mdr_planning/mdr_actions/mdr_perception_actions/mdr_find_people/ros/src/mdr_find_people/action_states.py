367393fb10c6f9ec76ac748f5eed3a2b8db15a55
Alex Mitrevski <aleksandar.mitrevski@h-brs.de>
2020-11-16 16:24:19 +0100

    [find_people] Replaced the SSD keras detector with a torch Faster R-CNN detector

--- mdr_planning/mdr_actions/mdr_perception_actions/mdr_find_people/ros/src/mdr_find_people/action_states.py
+++ mdr_planning/mdr_actions/mdr_perception_actions/mdr_find_people/ros/src/mdr_find_people/action_states.py
@@ -1,8 +1,11 @@
+from importlib import import_module
+from PIL import Image as PILImage
+import yaml
+
 import rospy
 import smach
 import torch
 
-from PIL import Image as PILImage
 import tf
 from sensor_msgs.msg import PointCloud2, Image
 from mdr_find_people.msg import FindPeopleResult
@@ -25,16 +28,44 @@ class FindPeopleState(smach.State):
 
         self._listener = tf.TransformListener()
         self.pointcloud_topic = rospy.get_param("~pointcloud_topic", '/rectified_points')
+        self.class_annotation_file = rospy.get_param('class_annotation_file', '')
+        self.detector_module = rospy.get_param('detector_module', 'torchvision.models.detection')
+        self.detector_name = rospy.get_param('detector_name', 'fasterrcnn_resnet50_fpn')
+        self.detection_threshold = float(rospy.get_param('detection_threshold', 0.))
         self.face_embedding_model_path = rospy.get_param("~face_embedding_model_path", '')
+
+        if not self.class_annotation_file:
+            raise ValueError('Parameter "class_annotation_file" cannot be empty!')
+
+        self.class_annotations = None
+        with open(self.class_annotation_file, 'r') as annotations_file:
+            self.class_annotations = yaml.safe_load(annotations_file)
+        self.class_annotations[0] = 'background'
+
+        if not self.detector_module:
+            rospy.logwarn('[find_people] Found empty "detector_module"; using default torchvision.models.detection')
+            self.detector_module = 'torchvision.models.detection'
+
+        if not self.detector_name:
+            rospy.logwarn('[find_people] Found empty "detector_name"; using default fasterrcnn_resnet50_fpn')
+            self.detector_module = 'fasterrcnn_resnet50_fpn'
+
+        self.model_device = torch.device('cuda') if torch.cuda.is_available() \
+                                                 else torch.device('cpu')
+        detector_class = getattr(import_module(self.detector_module),
+                                 self.detector_name)
+        self.detector = detector_class(pretrained=True)
+        self.detector.eval()
+        self.detector.to(self.model_device)
 
         self.face_embedding_model = None
         if self.face_embedding_model_path:
             self.face_embedding_model = SiameseNetwork()
             self.face_embedding_model.load_state_dict(torch.load(self.face_embedding_model_path))
             self.face_embedding_model.eval()
+            self.face_embedding_model.to(self.model_device)
 
-            device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-            self.face_embedding_model.to(device)
+        self.cv_bridge = CvBridge()
 
     def execute(self, userdata):
         rospy.loginfo('Executing state FIND_PEOPLE')
@@ -43,22 +74,25 @@ class FindPeopleState(smach.State):
         cloud_msg = rospy.wait_for_message(self.pointcloud_topic, PointCloud2)
 
         # Get positions of people
-        predictions, bb2ds, poses = FindPeople.detect(cloud_msg)
+        predictions, bb2ds, poses = FindPeople.detect(cloud_msg,
+                                                      self.detector,
+                                                      self.model_device,
+                                                      self.class_annotations,
+                                                      self.detection_threshold)
 
         # Get people images
         cv_image = cloud_msg_to_cv_image(cloud_msg)
-        bridge = CvBridge()
         images = []
         face_images = []
         for i, bb2d in enumerate(bb2ds):
             cropped_cv = crop_image(cv_image, bb2d)
-            cropped_img_msg = bridge.cv2_to_imgmsg(cropped_cv, encoding="passthrough")
+            cropped_img_msg = self.cv_bridge.cv2_to_imgmsg(cropped_cv, encoding="passthrough")
 
             rospy.loginfo('[find_people] Attempting to extract face of person {0}'.format(i+1))
             cropped_face_img = FindPeople.extract_face_image(cropped_cv)
             if cropped_face_img is not None:
-                cropped_face_img_msg = bridge.cv2_to_imgmsg(cropped_face_img,
-                                                            encoding='passthrough')
+                cropped_face_img_msg = self.cv_bridge.cv2_to_imgmsg(cropped_face_img,
+                                                                    encoding='passthrough')
             else:
                 cropped_face_img_msg = Image()
 
@@ -83,7 +117,7 @@ class FindPeopleState(smach.State):
             face_view.image = face_images[i]
 
             if self.face_embedding_model is not None and face_view.image.data:
-                face_cv2 = bridge.imgmsg_to_cv2(face_view.image)
+                face_cv2 = self.cv_bridge.imgmsg_to_cv2(face_view.image)
                 img_tensor = get_transforms()(PILImage.fromarray(face_cv2))
                 img_tensor.unsqueeze_(0)
 
