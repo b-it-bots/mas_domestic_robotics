740934514fd76c241f89416b33da3be3e24199dd
Sushant Chavan <gitecsvc@gmail.com>
2020-12-19 18:02:55 +0100

    Cleanup and fixes

--- mdr_perception/mdr_cloud_object_detection/ros/include/mdr_cloud_object_detection/CloudObjectDetection.h
+++ mdr_perception/mdr_cloud_object_detection/ros/include/mdr_cloud_object_detection/CloudObjectDetection.h
@@ -1,46 +1,42 @@
 /*!
- * @copyright 2018 Bonn-Rhein-Sieg University
+ * @copyright 2020 Bonn-Rhein-Sieg University
  *
  * @author Sushant Vijay Chavan
  *
  * @brief Class to detect objects from a point cloud
  */
+#ifndef MDR_CLOUD_OBJECT_DETECTION_H
+#define MDR_CLOUD_OBJECT_DETECTION_H
 
-#include <cmath>
 #include <actionlib/client/simple_action_client.h>
-#include <eigen_conversions/eigen_msg.h>
-#include <pcl_ros/transforms.h>
-#include <pcl_conversions/pcl_conversions.h>
-#include <pcl/segmentation/extract_clusters.h>
-#include <pcl/common/common.h>
-#include <pcl/features/moment_of_inertia_estimation.h>
 #include <dynamic_reconfigure/server.h>
-#include <nav_msgs/OccupancyGrid.h>
+#include <pcl/segmentation/extract_clusters.h>
+#include <pcl_ros/transforms.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <std_msgs/Bool.h>
-#include <tf/transform_listener.h>
-#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 #include <visualization_msgs/MarkerArray.h>
+
 #include <mas_navigation_tools/OccupancyCheckerAction.h>
 #include <mas_perception_msgs/ObjectList.h>
-#include "mdr_cloud_object_detection/Color.h"
-#include "mdr_cloud_object_detection/Utils.h"
+
+#include <mdr_cloud_object_detection/Utils.h>
+#include <mdr_cloud_object_detection/PassThroughVoxelFilter.h>
 #include <mdr_cloud_object_detection/ObjectDetectionConfig.h>
 
 namespace mdr_cloud_object_detection
 {
     class CloudObjectDetection
     {
-    private:
+    protected:
         ros::NodeHandle mNodeHandle;
+        actionlib::SimpleActionClient<mas_navigation_tools::OccupancyCheckerAction> mOccupancyCheckerClient;
         dynamic_reconfigure::Server<ObjectDetectionConfig> mObjectDetectionConfigServer;
-        ros::Subscriber mCloudSub;
-        ros::Subscriber mResetSub;
+        ros::Subscriber mCloudInSub;
+        ros::Subscriber mResetObjectCacheSub;
         ros::Publisher mFilteredCloudPub;
         ros::Publisher mObjectCloudPub;
         ros::Publisher mObjectBoundsPub;
-        ros::Publisher mObjectObjectsPub;
-        actionlib::SimpleActionClient<mas_navigation_tools::OccupancyCheckerAction> mOccupancyCheckerClient;
+        ros::Publisher mObjectListPub;
         tf::TransformListener mTfListener;
         std::string mTransformTargetFrame;
         std::string mClusterTargetFrame;
@@ -59,27 +55,23 @@ namespace mdr_cloud_object_detection
 
     public:
         CloudObjectDetection(const ros::NodeHandle &pNodeHandle,
-                             const std::string &pCloudTopic,
+                             const std::string &pCloudInTopic,
                              const std::string &pFilteredCloudTopic,
                              const std::string &pObjectCloudTopic,
                              const std::string &pTransformTargetFrame,
                              const std::string &pClusterTargetFrame,
                              const std::string &pOccupancyCheckerActionName,
                              const std::string &pObjectsBoundsTopic,
-                             const std::string &pObjectObjectsTopic,
+                             const std::string &pObjectListTopic,
                              bool pPublishOrientedBBox);
 
-    private:
+    protected:
         void objectDetectionConfigCallback(const ObjectDetectionConfig &pConfig,
                                            uint32_t pLevel);
 
         void cloudCallback(const sensor_msgs::PointCloud2::ConstPtr& pCloudMsgPtr);
 
-        void resetCallback(const std_msgs::Bool& reset);
-
-        void filterClusterCloudsBySize(std::vector<PointCloud::Ptr>& clusterClouds);
-
-        void filterClusterCloudsNearOccupiedSpaces(std::vector<PointCloud::Ptr>& clusterClouds);
+        void resetObjectCacheCallback(const std_msgs::Bool& reset);
 
         void getClusterClouds(std::vector<PointCloud::Ptr>& clusterClouds,
                               PointCloud::ConstPtr filteredCloud,
@@ -97,11 +89,22 @@ namespace mdr_cloud_object_detection
 
         void processNewClusters(const std::vector<PointCloud::Ptr>& clusterClouds);
 
+        void filterClusterCloudsBySize(std::vector<PointCloud::Ptr>& clusterClouds);
+
+        void filterClusterCloudsNearOccupiedSpaces(std::vector<PointCloud::Ptr>& clusterClouds);
+
         void removeStaleObjects();
 
-        geometry_msgs::Point getGeomPoint(float x,
-                                          float y,
-                                          float z);
+        mas_perception_msgs::Object createObjectMessage(PointCloud::ConstPtr cloudPtr,
+                                                        int id);
+
+        void updateObjectMsgCache();
+
+        const mas_perception_msgs::Object* getObjectMessage(int id);
+
+        geometry_msgs::Point getPointMsg(float x,
+                                         float y,
+                                         float z);
 
         visualization_msgs::Marker getObjectBoundsMarker(const Eigen::Vector4f& min,
                                                          const Eigen::Vector4f& max,
@@ -117,13 +120,6 @@ namespace mdr_cloud_object_detection
                                                      const Eigen::Vector4f& max,
                                                      int id);
 
-        mas_perception_msgs::Object createObjectMessage(PointCloud::ConstPtr cloudPtr,
-                                                        int id);
-
-        void updateObjectMsgCache();
-
-        const mas_perception_msgs::Object* getObjectMessage(int id);
-
         void publishCloud(PointCloud::ConstPtr cloudPtr,
                           const ros::Publisher& publisher);
 
@@ -131,9 +127,12 @@ namespace mdr_cloud_object_detection
 
         void publishObjectBoundsMarkers();
 
-        void publishObjectObjectMessages();
+        void publishObjectListMessages();
 
         void cleanup();
     };
 
 }   // namespace mdr_cloud_object_detection
+
+
+#endif  // MDR_CLOUD_OBJECT_DETECTION_H
