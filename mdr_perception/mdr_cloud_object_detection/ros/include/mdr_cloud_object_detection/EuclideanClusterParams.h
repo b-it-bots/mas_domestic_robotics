6cb830b703bd4dc3f5b49d5ba35983740dd9dabe
Sushant Chavan <gitecsvc@gmail.com>
2020-12-19 11:43:13 +0100

    Move individual classes to separate header/cpp files

---
+++ mdr_perception/mdr_cloud_object_detection/ros/include/mdr_cloud_object_detection/EuclideanClusterParams.h
@@ -0,0 +1,19 @@
+#ifndef MDR_CLOUD_OBJECT_DETECTION_EUCLIDEAN_CLUSTER_PARAMS
+#define MDR_CLOUD_OBJECT_DETECTION_EUCLIDEAN_CLUSTER_PARAMS
+
+#include <limits>
+
+namespace mdr_cloud_object_detection
+{
+    /*!
+    * @brief struct containing parameters necessary for clustering point clouds
+    */
+    struct EuclideanClusterParams
+    {
+        float mClusterTolerance = 0.01f;
+        unsigned int mMinClusterSize = 1;
+        unsigned int mMaxClusterSize = std::numeric_limits<unsigned int>::max();
+    };
+}
+
+#endif // MDR_CLOUD_OBJECT_DETECTION_EUCLIDEAN_CLUSTER_PARAMS
---
+++ mdr_perception/mdr_cloud_object_detection/ros/include/mdr_cloud_object_detection/ObjectCacheParams.h
@@ -0,0 +1,18 @@
+#ifndef MDR_CLOUD_OBJECT_DETECTION_OBJECT_CACHE_PARAMS
+#define MDR_CLOUD_OBJECT_DETECTION_OBJECT_CACHE_PARAMS
+
+namespace mdr_cloud_object_detection
+{
+    /*!
+    * @brief struct containing parameters necessary for caching the detected objects
+    */
+    struct ObjectCacheParams
+    {
+        float mObjectCacheTime = 5.0f;
+        float mSimilarityThreshold = 2.0f;
+        int mPositionHistoryCacheSize = 10;
+        float mUniquenessThreshold = 0.005f;
+    };
+}
+
+#endif // MDR_CLOUD_OBJECT_DETECTION_OBJECT_CACHE_PARAMS
