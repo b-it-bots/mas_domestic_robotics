e112b8c7a5a409e693ad014f02d34aa7016dc23e
AhmedFaisal95 <ahmedfaisal_95@hotmail.co.uk>
2021-06-12 11:19:10 +0200

    [wrs_tidy_up] Modify SelectObjectForPicking: check requested obj category, else get closest obj

--- mdr_planning/mdr_scenarios/mdr_robocup_tasks/mdr_wrs_tidy_up/ros/src/mdr_wrs_tidy_up/scenario_states/select_object_for_picking.py
+++ mdr_planning/mdr_scenarios/mdr_robocup_tasks/mdr_wrs_tidy_up/ros/src/mdr_wrs_tidy_up/scenario_states/select_object_for_picking.py
@@ -2,6 +2,8 @@ import tf
 import rospy
 import numpy as np
 
+from std_msgs.msg import String
+
 from mas_execution_manager.scenario_state_base import ScenarioStateBase
 
 class SelectObjectForPicking(ScenarioStateBase):
@@ -12,13 +14,45 @@ class SelectObjectForPicking(ScenarioStateBase):
                                    output_keys=['selected_object'],
                                    outcomes=['succeeded'])
         self.tf_listener = tf.TransformListener()
+        self.requested_object_category_topic = kwargs.get('requested_object_category_topic ',
+                                                  '/message')
         self.sm_id = kwargs.get('sm_id', '')
         self.state_name = kwargs.get('state_name', 'select_object_for_picking')
 
+        self.requested_object_category = None
+
+        self.__init_ros_components()
+
     def execute(self, userdata):
+        if self.requested_object_category is None:
+            rospy.loginfo('[%s] Selecting closest object...', self.state_name)
+            selected_object = self.get_closest_object(userdata.detected_objects)
+        else:
+            for obj in userdata.detected_objects:
+                if obj.category == self.requested_object_category:
+                    rospy.loginfo('[%s] Found requested object among detected objects!', self.state_name)
+                    rospy.loginfo('[%s] Selecting requested object: %s', self.state_name, self.requested_object_category)
+                    selected_object = obj
+                    break
+            else:
+                rospy.warn('[%s] Could not find requested object among detected objects!', self.state_name)
+                rospy.loginfo('[%s] Defaulting to selecting closest object...', self.state_name)
+                selected_object = self.get_closest_object(userdata.detected_objects)
+
+        rospy.loginfo('[%s] Selected object at position\n    (%f, %f, %f) \n with size\n    (%f, %f, %f)',
+                      self.state_name, selected_object.pose.pose.position.x,
+                      selected_object.pose.pose.position.y,
+                      selected_object.pose.pose.position.z,
+                      selected_object.dimensions.vector.x,
+                      selected_object.dimensions.vector.y,
+                      selected_object.dimensions.vector.z)
+        userdata.selected_object = selected_object
+        return 'succeeded'
+
+    def get_closest_object(self, detected_objects):
         closest_obj_index = 0
         closest_obj_distance = float('inf')
-        for index, obj in enumerate(userdata.detected_objects):
+        for index, obj in enumerate(detected_objects):
             try:
                 pose_base_link = self.tf_listener.transformPose('base_link', obj.pose)
             except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException) as exc:
@@ -33,13 +67,20 @@ class SelectObjectForPicking(ScenarioStateBase):
                 closest_obj_index = index
                 closest_obj_distance = distance_to_obj
 
-        selected_object = userdata.detected_objects[closest_obj_index]
-        rospy.loginfo('[%s] Selected object at position\n    (%f, %f, %f) \n with size\n    (%f, %f, %f)',
-                      self.state_name, selected_object.pose.pose.position.x,
-                      selected_object.pose.pose.position.y,
-                      selected_object.pose.pose.position.z,
-                      selected_object.dimensions.vector.x,
-                      selected_object.dimensions.vector.y,
-                      selected_object.dimensions.vector.z)
-        userdata.selected_object = selected_object
-        return 'succeeded'
+        return detected_objects[closest_obj_index]
+
+    def requested_object_category_callback(self, msg):
+        rospy.loginfo('[%s] Received requested object category: %s', self.state_name, msg.data)
+        self.requested_object_category = msg.data
+
+    def __init_ros_components(self):
+        '''Initialises:
+        * a subscriber for the topic on which the category of a requested object is published
+        '''
+        rospy.loginfo('[%s] Creating subscriber for topic %s',
+                      self.state_name, self.requested_object_category_topic)
+        self.obstacle_sub = rospy.Subscriber(self.requested_object_category_topic,
+                                             String, self.requested_object_category_callback)
+        rospy.sleep(0.5)
+        rospy.loginfo('[%s] Subscriber for topic %s created',
+                      self.state_name, self.requested_object_category_topic)
