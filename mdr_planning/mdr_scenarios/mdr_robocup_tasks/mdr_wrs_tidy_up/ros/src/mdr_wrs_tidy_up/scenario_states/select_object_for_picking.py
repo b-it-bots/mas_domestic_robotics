f14c2f353dcaa69e3a16339c8c0ac191ebd4beda
AhmedFaisal95 <ahmedfaisal_95@hotmail.co.uk>
2021-06-17 17:48:24 +0200

    [wrs_tidy_up] Modify SelectObjectForPicking for updated object request message string

--- mdr_planning/mdr_scenarios/mdr_robocup_tasks/mdr_wrs_tidy_up/ros/src/mdr_wrs_tidy_up/scenario_states/select_object_for_picking.py
+++ mdr_planning/mdr_scenarios/mdr_robocup_tasks/mdr_wrs_tidy_up/ros/src/mdr_wrs_tidy_up/scenario_states/select_object_for_picking.py
@@ -1,3 +1,5 @@
+import random
+
 import tf
 import rospy
 import numpy as np
@@ -11,23 +13,35 @@ class SelectObjectForPicking(ScenarioStateBase):
         ScenarioStateBase.__init__(self, 'select_object_for_picking',
                                    save_sm_state=save_sm_state,
                                    input_keys=['detected_objects'],
-                                   output_keys=['selected_object'],
+                                   output_keys=['selected_object',
+                                                'destination_locations'],
                                    outcomes=['succeeded'])
         self.tf_listener = tf.TransformListener()
         self.requested_object_category_topic = kwargs.get('requested_object_category_topic ',
-                                                  '/message')
+                                                          '/message')
         self.sm_id = kwargs.get('sm_id', '')
         self.state_name = kwargs.get('state_name', 'select_object_for_picking')
 
+        self.object_request_string = None
         self.requested_object_category = None
+        self.requesting_person = None
 
         self.__init_ros_components()
 
     def execute(self, userdata):
-        if self.requested_object_category is None:
+        if self.object_request_string is None:
             rospy.loginfo('[%s] Selecting closest object...', self.state_name)
             selected_object = self.get_closest_object(userdata.detected_objects)
         else:
+            self.requested_object_category = self.object_request_string.split(' ')[0]
+            person_string = '_'.join(self.object_request_string.split(' ')[-2:])
+            if person_string in ['person_right', 'person_left']:
+                self.requesting_person = person_string
+            else:
+                rospy.logwarn('[%s] Invalid person specification! Picking one at random', self.state_name)
+                self.requesting_person = random.choice(['person_right', 'person_left'])
+
+            rospy.loginfo('[%s] Selecting %s to deliver to %s', self.state_name, self.requested_object_category, self.requesting_person)
             for obj in userdata.detected_objects:
                 if obj.category == self.requested_object_category:
                     rospy.loginfo('[%s] Found requested object among detected objects!', self.state_name)
@@ -35,9 +49,11 @@ class SelectObjectForPicking(ScenarioStateBase):
                     selected_object = obj
                     break
             else:
-                rospy.warn('[%s] Could not find requested object among detected objects!', self.state_name)
+                rospy.logwarn('[%s] Could not find requested object among detected objects!', self.state_name)
                 rospy.loginfo('[%s] Defaulting to selecting closest object...', self.state_name)
                 selected_object = self.get_closest_object(userdata.detected_objects)
+
+            userdata.destination_locations = [self.requesting_person]
 
         rospy.loginfo('[%s] Selected object at position\n    (%f, %f, %f) \n with size\n    (%f, %f, %f)',
                       self.state_name, selected_object.pose.pose.position.x,
@@ -70,8 +86,8 @@ class SelectObjectForPicking(ScenarioStateBase):
         return detected_objects[closest_obj_index]
 
     def requested_object_category_callback(self, msg):
-        rospy.loginfo('[%s] Received requested object category: %s', self.state_name, msg.data)
-        self.requested_object_category = msg.data
+        rospy.loginfo('[%s] Received object request: %s', self.state_name, msg.data)
+        self.object_request_string = msg.data
 
     def __init_ros_components(self):
         '''Initialises:
